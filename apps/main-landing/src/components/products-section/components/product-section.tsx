083a7297576700aaf0155595c145451a428efbb6
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2024-12-21 19:50:19 +0100

    wip

--- apps/main-landing/src/components/products-section/components/product-section.tsx
+++ apps/main-landing/src/components/products-section/components/product-section.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 import { Icon, IconName } from '@idriss-xyz/ui/icon';
-import { CSSProperties, ReactNode } from 'react';
+import { ReactNode } from 'react';
 import { classes } from '@idriss-xyz/ui/utils';
 
 import { ImageSequencer } from '@/components/image-sequencer';
@@ -22,7 +22,9 @@ type ProductSectionProperties = {
   headerClassName?: string;
   fadeOut: boolean;
   tabsVisibile?: boolean;
-  style?: CSSProperties;
+  marginX?: number;
+  marginY?: number;
+  borderRadius?: number;
 } & (
   | {
       animated: true;
@@ -58,7 +60,9 @@ export const ProductSection = ({
   features,
   tabsAsLinks,
   fadeOut,
-  style,
+  borderRadius,
+  marginX,
+  marginY,
   tabsVisibile = true,
   ...properties
 }: ProductSectionProperties) => {
@@ -71,12 +75,14 @@ export const ProductSection = ({
     >
       <div
         className={classes(
-          'flex size-full flex-col overflow-hidden bg-[linear-gradient(114deg,_#022B1E_34.81%,_#079165_123.57%)] px-4 transition-[border-radius] duration-150 will-change-[border-radius]',
-          'lg:px-[120px] lg:pt-20',
-          '2xl:px-[112px] 2xl:py-20',
+          'relative flex size-full flex-col overflow-hidden bg-[linear-gradient(114deg,_#022B1E_34.81%,_#079165_123.57%)] px-4 will-change-[border-radius,margin]',
+          'lg:pt-20',
           '4xl:px-[180px] 4xl:py-[120px]',
         )}
-        style={style}
+        style={{
+          margin: `${marginY}px ${marginX}px`,
+          borderRadius: `${borderRadius}px`,
+        }}
       >
         <div
           className={classes(
@@ -84,38 +90,15 @@ export const ProductSection = ({
             'lg:gap-[104px]',
           )}
         >
-          {properties.animated ? (
-            <ImageSequencer
-              infinite={false}
-              images={properties.animationImages}
-              direction={properties.animationDirection}
-              startIndex={properties.animationStartIndex}
-              endIndex={properties.animationEndIndex}
-              className={classes(
-                'bottom-0 right-0 top-1/2 z-0',
-                'lg:absolute lg:max-w-[45%] lg:-translate-y-1/2',
-                '2xl:right-[-220px] 2xl:top-[400px] 2xl:w-[780px] 2xl:max-w-[unset]',
-              )}
-            />
-          ) : (
-            <img
-              src={properties.circleImage}
-              alt=""
-              className={classes(
-                'bottom-0 right-0 top-1/2 z-0 m-auto w-[320px] translate-y-[-15%]',
-                'md:w-[450px] md:translate-y-0',
-                'lg:absolute lg:max-w-[45%] lg:-translate-y-1/2',
-              )}
-            />
-          )}
           <div
             className={classes(
               'flex flex-col items-start gap-[64px]',
               'md:gap-10',
-              'left-[112px] w-screen lg:absolute',
+              'lg:absolute lg:w-screen lg:will-change-[left]',
               '2xl:gap-6',
               '3xl:gap-10',
             )}
+            style={{ left: `${112 - (marginX ?? 0) / 2}px` }}
           >
             {tabsVisibile && (
               <Tabs
@@ -179,7 +162,30 @@ export const ProductSection = ({
                 {actions}
               </div>
             </div>
-
+            {properties.animated ? (
+              <ImageSequencer
+                infinite={false}
+                images={properties.animationImages}
+                direction={properties.animationDirection}
+                startIndex={properties.animationStartIndex}
+                endIndex={properties.animationEndIndex}
+                className={classes(
+                  'bottom-0 right-0 top-1/2 z-0',
+                  'lg:absolute lg:max-w-[45%] lg:-translate-y-1/2',
+                  '2xl:right-[-40px] 2xl:top-[310px] 2xl:w-[780px] 2xl:max-w-[unset]',
+                )}
+              />
+            ) : (
+              <img
+                src={properties.circleImage}
+                alt=""
+                className={classes(
+                  'bottom-0 right-0 top-1/2 z-0 m-auto w-[320px] translate-y-[-15%]',
+                  'md:w-[450px] md:translate-y-0',
+                  'lg:absolute lg:max-w-[45%] lg:-translate-y-1/2',
+                )}
+              />
+            )}
             <div className="z-1 mx-auto mt-[-80px] overflow-hidden md:mt-0 lg:mx-0">
               <div
                 className={classes(
