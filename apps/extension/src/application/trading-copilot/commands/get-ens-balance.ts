919b24ea0f226682ccc3a5eeace6487cefa2ac59
Mateusz Stawski <mateuszmstawski@gmail.com>
2024-12-27 12:19:51 +0100

    fix: add transport url to other two commands using public client

--- apps/extension/src/application/trading-copilot/commands/get-ens-balance.ts
+++ apps/extension/src/application/trading-copilot/commands/get-ens-balance.ts
@@ -25,7 +25,7 @@ export class GetEnsBalanceCommand extends Command<Payload, string | null> {
     try {
       const client = createPublicClient({
         chain: { ...mainnet },
-        transport: http(),
+        transport: http('https://eth.llamarpc.com'),
       });
 
       const result = await client.getBalance(this.payload);
