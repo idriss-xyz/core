097c4e167a1aac662a4d09133ea47d132b1ebc48
21142 <s21142@pjwstk.edu.pl>
2025-01-06 17:03:48 +0100

    adjusted the new token section to the final version

--- apps/main-landing/src/components/token-section/token-section.tsx
+++ apps/main-landing/src/components/token-section/token-section.tsx
@@ -28,7 +28,8 @@ export const TokenSection = () => {
               'relative flex w-full flex-col items-center gap-[15px] rounded-[36px] bg-white/50 px-4 py-10 backdrop-blur-[45px]',
               'md:px-10 lg:w-[893px]',
               'lg:px-[54px] lg:py-[60px]',
-              '2xl:w-[803px] 2xl:px-10 2xl:pb-[60px] 2xl:pt-10',
+              '2xl:px-10 2xl:pb-[60px] 2xl:pt-10',
+              '3xl:px-8',
             )}
           >
             <GradientBorder
@@ -61,7 +62,7 @@ export const TokenSection = () => {
               className={classes(
                 'mb-2 mt-4 text-balance text-center text-body3 text-neutralGreen-900 opacity-70',
                 'lg:text-body2',
-                '4xl:text-body1',
+                '4xl:text-body',
               )}
             >
               IDRISS is the utility token powering{'\u00A0'}the IDRISS{'\u00A0'}
@@ -70,113 +71,73 @@ export const TokenSection = () => {
               decentralized{'\u00A0'}revenue sharing, governance rights, and
               more.
             </p>
-            {/* Mobile */}
-            <div
-              className={classes(
-                'mb-3 flex flex-row justify-center gap-6',
-                'md:mb-4',
-                'lg:hidden',
-              )}
-            >
-              <Button
-                intent="tertiary"
-                size="medium"
-                suffixIconName="IdrissArrowRight"
-                asLink
-                href="https://docs.idriss.xyz/idriss-token/token-sale#faq"
-                isExternal
-              >
-                FAQ
-              </Button>
-              <Button
-                intent="tertiary"
-                size="medium"
-                suffixIconName="IdrissArrowRight"
-                asLink
-                href="https://docs.idriss.xyz/idriss-token"
-                isExternal
-              >
-                TOKENOMICS
-              </Button>
-            </div>
-            {/* Desktop */}
-            <div className="hidden flex-row justify-normal gap-6 lg:flex">
-              <Button
-                intent="tertiary"
-                size="large"
-                suffixIconName="IdrissArrowRight"
-                asLink
-                href="https://docs.idriss.xyz/idriss-token/token-sale#faq"
-                isExternal
-              >
-                FAQ
-              </Button>
-              <Button
-                intent="tertiary"
-                size="large"
-                suffixIconName="IdrissArrowRight"
-                asLink
-                href="https://docs.idriss.xyz/idriss-token"
-                isExternal
-              >
-                TOKENOMICS
-              </Button>
-            </div>
-            <div className="flex flex-col gap-6 lg:gap-4">
-              <div className="flex flex-col justify-center gap-6 md:flex-row">
-                <GeoConditionalButton
-                  defaultButton={[
-                    <Button
-                      key="uniswap"
-                      intent="primary"
-                      size="large"
-                      prefixIconName="Uniswap"
-                      asLink
-                      href="https://app.uniswap.org/swap?inputCurrency=0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913&outputCurrency=0x000096630066820566162C94874A776532705231"
-                      isExternal
-                      className="w-full md:w-auto"
-                    >
-                      BUY ON UNISWAP
-                    </Button>,
-                    <Button
-                      key="jumper"
-                      intent="primary"
-                      size="large"
-                      prefixIconName="Jumper"
-                      asLink
-                      href="https://jumper.exchange/?fromChain=8453&fromToken=0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913&toChain=8453&toToken=0x000096630066820566162C94874A776532705231"
-                      isExternal
-                      className="w-full whitespace-nowrap md:w-auto"
-                    >
-                      BUY CROSS-CHAIN ON JUMPER
-                    </Button>,
-                  ]}
-                />
+            <div className="flex-row justify-normal gap-6 lg:flex">
+              <div className="flex flex-col gap-6 lg:gap-4">
+                <div className="flex flex-col items-center justify-center gap-6 lg:flex-row">
+                  <GeoConditionalButton
+                    defaultButton={[
+                      <Button
+                        key="uniswap"
+                        intent="primary"
+                        size="large"
+                        prefixIconName="Uniswap"
+                        asLink
+                        href="https://app.uniswap.org/swap?inputCurrency=0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913&outputCurrency=0x000096630066820566162C94874A776532705231"
+                        isExternal
+                        className="mx-auto w-full md:w-auto"
+                      >
+                        BUY ON UNISWAP
+                      </Button>,
+                      <Button
+                        key="jumper"
+                        intent="primary"
+                        size="large"
+                        prefixIconName="Jumper"
+                        asLink
+                        href="https://jumper.exchange/?fromChain=8453&fromToken=0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913&toChain=8453&toToken=0x000096630066820566162C94874A776532705231"
+                        isExternal
+                        className="w-full whitespace-nowrap md:w-auto"
+                      >
+                        BUY ON JUMPER
+                      </Button>,
+                    ]}
+                  />
+                  <Button
+                    intent="tertiary"
+                    size="large"
+                    suffixIconName="IdrissArrowRight"
+                    asLink
+                    href="https://docs.idriss.xyz/idriss-token"
+                    isExternal
+                  >
+                    TOKENOMICS
+                  </Button>
+                </div>
+                <div className="self-stretch text-center opacity-70">
+                  <span
+                    className={classes(
+                      'text-body4 text-neutralGreen-900',
+                      'md:text-body3',
+                    )}
+                  >
+                    By purchasing, you agree to the{' '}
+                  </span>
+                  <Link
+                    size="medium"
+                    href={TOKEN_TERMS_AND_CONDITIONS_LINK}
+                    isExternal
+                    className={classes(
+                      'text-body4',
+                      'md:text-body3',
+                      //lg here is intentional to override the Link variant style
+                      'lg:text-body3',
+                    )}
+                  >
+                    Terms{'\u00A0'}and{'\u00A0'}conditions
+                  </Link>
+                </div>
+                <div className="flex justify-center" />
               </div>
-              <div className="self-stretch text-center opacity-70">
-                <span
-                  className={classes(
-                    'text-body4 text-neutralGreen-900',
-                    'md:text-body2',
-                  )}
-                >
-                  By purchasing, you agree to the{' '}
-                </span>
-                <Link
-                  size="medium"
-                  href={TOKEN_TERMS_AND_CONDITIONS_LINK}
-                  isExternal
-                  className={classes(
-                    'text-body4',
-                    'md:text-body2',
-                    //lg here is intentional to override the Link variant style
-                    'lg:text-body2',
-                  )}
-                >
-                  Terms{'\u00A0'}and{'\u00A0'}conditions
-                </Link>
-              </div>
-              <div className="flex justify-center" />
             </div>
           </div>
           <img
