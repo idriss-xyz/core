53ca1fe6793fc1e2d10a94044ae5706dc59efc6d
Maciej Kutynia <kutynia.maciej@gmail.com>
2025-01-06 20:30:52 +0100

    resolve conflicts

--- apps/copilot-api/constants.ts
+++ apps/copilot-api/constants.ts
@@ -4,4 +4,4 @@ export const NULL_ADDRESS = '0x0000000000000000000000000000000000000000';
 export const webhooks: Webhook[] = [];
 export const MAX_ADDRESSES_PER_WEBHOOK = 3; // Adjust based on Alchemy's limits - 50000
 
-export const WEBHOOK_NETWORKS = ['BASE_MAINNET', 'ETH_MAINNET', 'OPT_MAINNET'];
+export const WEBHOOK_NETWORKS = ['BASE_MAINNET'];
