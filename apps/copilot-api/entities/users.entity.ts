0c0ae778c871b2d3a0016049ccd09965646683ea
Maciej Kutynia <kutynia.maciej@gmail.com>
2025-01-06 20:10:14 +0100

    Copilot api (#223)

--- apps/copilot-api/entities/users.entity.ts
+++ apps/copilot-api/entities/users.entity.ts
@@ -1,7 +1,7 @@
-import {Entity, PrimaryColumn, PrimaryGeneratedColumn} from "typeorm";
+import { Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('users')
 export class UsersEntity {
-    @PrimaryGeneratedColumn({type: 'integer', name: 'uuid'})
-    readonly uuid!: number;
+  @PrimaryGeneratedColumn({ type: 'integer', name: 'uuid' })
+  readonly uuid!: number;
 }
--- apps/copilot-api/entities/webhook.entity.ts
+++ apps/copilot-api/entities/webhook.entity.ts
@@ -1,14 +1,13 @@
-import {Column, Entity, PrimaryColumn} from "typeorm";
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity('webhooks')
 export class WebhookEntity {
-    @PrimaryColumn({type: 'uuid', name: 'internal_id'})
-    readonly internal_id!: string
+  @PrimaryColumn({ type: 'uuid', name: 'internal_id' })
+  readonly internal_id!: string;
 
-    @Column({type: 'text', name: 'webhook_id'})
-    readonly webhook_id!: string
+  @Column({ type: 'text', name: 'webhook_id' })
+  readonly webhook_id!: string;
 
-    @Column({type: 'text', name: 'signing_key'})
-    readonly signing_key!: string
+  @Column({ type: 'text', name: 'signing_key' })
+  readonly signing_key!: string;
 }
-
