1307d98f595ed7e8305428e8f46351bfce81f832
Maciej Kutynia <kutynia.maciej@gmail.com>
2025-01-08 20:45:26 +0100

    fix missing alchemy header (#241)

--- apps/copilot-api/utils/webhookUtils.ts
+++ apps/copilot-api/utils/webhookUtils.ts
@@ -7,7 +7,7 @@ export function validateAlchemySignature(
 ) {
   return async (req: Request, res: Response, next: NextFunction) => {
     const rawBody = (req as any).rawBody as string;
-    const signature = (req as any).signature as string | undefined;
+    const signature = req.headers['x-alchemy-signature'] as string | undefined;
     const internalWebhookId = req.params.internalWebhookId;
 
     if (!signature) {
