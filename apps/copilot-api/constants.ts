24dcd23cbaaddc76d5af7d4e1ac78748ed6a5eca
Maciej Kutynia <kutynia.maciej@gmail.com>
2025-01-09 12:51:27 +0100

    change max addresses per webhook

--- apps/copilot-api/constants.ts
+++ apps/copilot-api/constants.ts
@@ -2,7 +2,7 @@ import { Webhook } from './interfaces';
 
 export const NULL_ADDRESS = '0x0000000000000000000000000000000000000000';
 export const webhooks: Webhook[] = [];
-export const MAX_ADDRESSES_PER_WEBHOOK = 3; // Adjust based on Alchemy's limits - 50000
+export const MAX_ADDRESSES_PER_WEBHOOK = 10000; // Adjust based on Alchemy's limits - 50000
 export const BLOCK_LIST = ['ETH', 'USDC', 'USDT', 'DAI'];
 
 export const WEBHOOK_NETWORKS = ['BASE_MAINNET'];
