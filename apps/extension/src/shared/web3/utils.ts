9d2ad982c1aa125378a5b4e28cba8439b571c8a5
Maciej Kutynia <kutynia.maciej@gmail.com>
2025-01-10 19:38:08 +0100

    fix number rounding, add tooltip to

--- apps/extension/src/shared/web3/utils.ts
+++ apps/extension/src/shared/web3/utils.ts
@@ -62,9 +62,19 @@ export const formatBigNumber = (number: number): string => {
 export const roundToSignificantFigures = (
   number: number,
   significantFigures: number,
-): number => {
+): number | string => {
   if (number === 0) {
     return 0;
+  }
+
+  if (number >= 1_000_000_000) {
+    return `${(number / 1_000_000_000).toFixed(significantFigures)}B`;
+  }
+  if (number >= 1_000_000) {
+    return `${(number / 1_000_000).toFixed(significantFigures)}M`;
+  }
+  if (number >= 1000) {
+    return `${(number / 1000).toFixed(significantFigures)}K`;
   }
 
   const multiplier = Math.pow(
