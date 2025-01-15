d22e539557f4934502e6652915f84d909ed78633
mkutynia <kutynia.maciej@gmail.com>
2025-01-15 19:47:42 +0100

    fix urls to copilot server

--- apps/extension/src/application/trading-copilot/commands/constants.ts
+++ apps/extension/src/application/trading-copilot/commands/constants.ts
@@ -1,2 +1,1 @@
-// export const COPILOT_API_URL = 'https://copilot-api.idriss.xyz';
-export const COPILOT_API_URL = ' https://e605-45-11-60-77.ngrok-free.app';
+export const COPILOT_API_URL = 'https://copilot-api.idriss.xyz';
