6db0bb8684a7f5f1110e1b30af02ff98af865f1a
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2025-01-19 20:37:57 +0100

    updated protected routes

--- apps/main-landing/src/middleware.ts
+++ apps/main-landing/src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const protectedRoutes = ['/check-eligibility', '/claim'];
+const protectedRoutes = ['/stake', '/claim'];
 
 export function middleware(request: NextRequest) {
   const url = request.nextUrl;
