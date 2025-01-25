071cd233282d1c92e9e3669ce62b212fc50d5aab
Daniel Chutkowski <daniel.chutkowski@gmail.com>
2025-01-25 17:38:20 +0100

    format

--- packages/ui/src/components/steps/steps.tsx
+++ packages/ui/src/components/steps/steps.tsx
@@ -16,36 +16,38 @@ export const Steps = ({ steps, activeStepIndex, className }: Properties) => {
     <div
       className={classes('flex w-full items-center justify-between', className)}
     >
-      {steps.map((step, index) => {return (
-        <div
-          className={classes(
-            'flex items-center',
-            index < steps.length - 1 && 'flex-grow',
-          )}
-          key={index}
-        >
+      {steps.map((step, index) => {
+        return (
           <div
             className={classes(
-              'flex min-w-6 items-center justify-center rounded bg-neutral-400 px-1 text-center text-label6 text-neutral-300',
-              activeStepIndex === index && 'bg-neutral-900 text-white',
+              'flex items-center',
+              index < steps.length - 1 && 'flex-grow',
             )}
+            key={index}
           >
-            {step.step}
-          </div>
-          <span
-            className={classes(
-              'ml-2 text-body4 text-neutral-600',
-              activeStepIndex === index && 'ml-2 text-body4 text-neutral-900',
-            )}
-          >
-            {step.title}
-          </span>
+            <div
+              className={classes(
+                'flex min-w-6 items-center justify-center rounded bg-neutral-400 px-1 text-center text-label6 text-neutral-300',
+                activeStepIndex === index && 'bg-neutral-900 text-white',
+              )}
+            >
+              {step.step}
+            </div>
+            <span
+              className={classes(
+                'ml-2 text-body4 text-neutral-600',
+                activeStepIndex === index && 'ml-2 text-body4 text-neutral-900',
+              )}
+            >
+              {step.title}
+            </span>
 
-          {index < steps.length - 1 && (
-            <div className="mx-4 h-px grow bg-neutral-500" />
-          )}
-        </div>
-      )})}
+            {index < steps.length - 1 && (
+              <div className="mx-4 h-px grow bg-neutral-500" />
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
