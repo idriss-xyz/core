0a80788eec94d8932cd46e638a517460cf5d6c48
macG <80455577+21142@users.noreply.github.com>
2025-01-27 19:22:39 +0100

    fix: updated after review (#354)

--- apps/main-landing/src/app/claim/components/claim-successful/claim-successful-content.tsx
+++ apps/main-landing/src/app/claim/components/claim-successful/claim-successful-content.tsx
@@ -17,7 +17,8 @@ import idrissCoinsCircle from './assets/IDRISS_SCENE_CIRCLE_2 2.png';
 import { SOCIALS } from './constants';
 
 export const ClaimSuccessfulContent = () => {
-  const { setCurrentContent, eligibilityData, vestingPlan } = useClaimPage();
+  const { setCurrentContent, eligibilityData, vestingPlan, hasAlreadyClaimed } =
+    useClaimPage();
   const downloadAreaReference = useRef<HTMLDivElement>(null);
 
   if (!eligibilityData) {
@@ -32,63 +33,67 @@ export const ClaimSuccessfulContent = () => {
         gradientStopColor="rgba(145, 206, 154, 0.50)"
         borderWidth={1}
       />
-      <span className="text-heading4 text-neutral-900">CLAIM SUCCESSFUL</span>
-      <div
-        ref={downloadAreaReference}
-        className="relative flex h-[354px] w-[480px] flex-col items-center justify-center gap-6 self-stretch overflow-hidden rounded-2xl bg-mint-100 p-6"
-      >
-        <img
-          alt=""
-          src={idrissCoinsCircle.src}
-          className="pointer-events-none absolute left-0 top-0"
-        />
-        <IconButton
-          size="large"
-          iconName="Download"
-          intent="tertiary"
-          iconClassName="size-6"
-          onMouseDown={async () => {
-            if (!downloadAreaReference.current) {
-              return;
-            }
+      <span className="text-heading4 text-neutral-900">
+        {hasAlreadyClaimed ? 'TOKENS ALREADY CLAIMED' : 'CLAIM SUCCESSFUL'}
+      </span>
+      {!hasAlreadyClaimed && (
+        <div
+          ref={downloadAreaReference}
+          className="relative flex h-[354px] w-[480px] flex-col items-center justify-center gap-6 self-stretch overflow-hidden rounded-2xl bg-mint-100 p-6"
+        >
+          <img
+            alt=""
+            src={idrissCoinsCircle.src}
+            className="pointer-events-none absolute left-0 top-0"
+          />
+          <IconButton
+            size="large"
+            iconName="Download"
+            intent="tertiary"
+            iconClassName="size-6"
+            onMouseDown={async () => {
+              if (!downloadAreaReference.current) {
+                return;
+              }
 
-            const dataUrl = await toPng(downloadAreaReference.current, {
-              pixelRatio: 5,
-            });
-            const link = document.createElement('a');
-            link.href = dataUrl;
-            link.setAttribute('download', `claim-successful.png`);
-            document.body.append(link);
-            link.click();
-            link.remove();
-          }}
-          className="absolute right-6 top-6 flex size-6 p-0 text-mint-500 active:opacity-0"
-        />
-        <div className="relative flex w-full flex-row justify-center" />
-        <div className="flex flex-col items-center gap-2">
-          <span className="text-body4 text-neutralGreen-700">
-            {vestingPlan === 'claim_50'
-              ? 'YOU RECEIVED'
-              : 'UNLOCK ON JULY 6, 2025'}
-          </span>
-          <div className="z-10 flex flex-col items-center justify-center rounded-[12px] border-[0.683px] border-[rgba(85,235,60,0.30)] bg-[radial-gradient(50%_50%_at_50%_50%,_rgba(252,255,242,0.00)_0%,_rgba(23,255,74,0.18)_100%)] px-10 py-5.5">
-            <span className="text-heading3 gradient-text">
-              +
-              {new Intl.NumberFormat('en-US', {
-                minimumFractionDigits: 0,
-                maximumFractionDigits: 0,
-              }).format(
-                Number(
-                  vestingPlan === 'claim_50'
-                    ? (eligibilityData.allocation ?? 0) / 2
-                    : (eligibilityData.allocation ?? 0),
-                ),
-              )}{' '}
-              $IDRISS
+              const dataUrl = await toPng(downloadAreaReference.current, {
+                pixelRatio: 5,
+              });
+              const link = document.createElement('a');
+              link.href = dataUrl;
+              link.setAttribute('download', `claim-successful.png`);
+              document.body.append(link);
+              link.click();
+              link.remove();
+            }}
+            className="absolute right-6 top-6 flex size-6 p-0 text-mint-500 active:opacity-0"
+          />
+          <div className="relative flex w-full flex-row justify-center" />
+          <div className="flex flex-col items-center gap-2">
+            <span className="text-body4 text-neutralGreen-700">
+              {vestingPlan === 'claim_50'
+                ? 'YOU RECEIVED'
+                : 'UNLOCK ON JULY 6, 2025'}
             </span>
+            <div className="z-10 flex flex-col items-center justify-center rounded-[12px] border-[0.683px] border-[rgba(85,235,60,0.30)] bg-[radial-gradient(50%_50%_at_50%_50%,_rgba(252,255,242,0.00)_0%,_rgba(23,255,74,0.18)_100%)] px-10 py-5.5">
+              <span className="text-heading3 gradient-text">
+                +
+                {new Intl.NumberFormat('en-US', {
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                }).format(
+                  Number(
+                    vestingPlan === 'claim_50'
+                      ? (eligibilityData.allocation ?? 0) / 2
+                      : (eligibilityData.allocation ?? 0),
+                  ),
+                )}{' '}
+                $IDRISS
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+      )}
       {vestingPlan === 'claim_50' ? (
         <GeoConditionalButton
           defaultButton={
@@ -106,7 +111,7 @@ export const ClaimSuccessfulContent = () => {
       ) : (
         <div className="flex w-full flex-col gap-4">
           <GeoConditionalButton
-            defaultButton={
+            defaultButton={[
               <Button
                 key="uniswap"
                 intent="primary"
@@ -118,11 +123,7 @@ export const ClaimSuccessfulContent = () => {
                 className="w-full"
               >
                 BUY MORE ON UNISWAP
-              </Button>
-            }
-          />
-          <GeoConditionalButton
-            defaultButton={
+              </Button>,
               <Button
                 key="jumper"
                 intent="primary"
@@ -134,8 +135,9 @@ export const ClaimSuccessfulContent = () => {
                 className="w-full"
               >
                 BUY MORE ON JUMPER
-              </Button>
-            }
+              </Button>,
+            ]}
+            additionalClasses="md:flex-col"
           />
           <div className="flex w-full items-center justify-center opacity-70">
             <span
