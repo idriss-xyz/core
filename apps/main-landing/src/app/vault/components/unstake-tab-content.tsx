00b886ba7528f137b2859d655a75107942553695
Levertz <95511599+lennardevertz@users.noreply.github.com>
2025-01-28 21:19:54 +0100

    - use 0 IDRISS as default (#366)

--- apps/main-landing/src/app/vault/components/unstake-tab-content.tsx
+++ apps/main-landing/src/app/vault/components/unstake-tab-content.tsx
@@ -207,7 +207,7 @@ export const UnstakeTabContent = () => {
                 {totalLockedAmount} IDRISS
               </p>
             ) : (
-              <Spinner className="size-4" />
+              <p className="text-label3 text-neutralGreen-700">0 IDRISS</p>
             )}
           </div>
           <div className="flex flex-row items-center justify-between">
@@ -217,7 +217,7 @@ export const UnstakeTabContent = () => {
                 {stakedAmount} IDRISS
               </p>
             ) : (
-              <Spinner className="size-4" />
+              <p className="text-label3 text-neutralGreen-700">0 IDRISS</p>
             )}
           </div>
           <div className="flex flex-row items-center justify-between">
@@ -229,7 +229,7 @@ export const UnstakeTabContent = () => {
                 {blockedAmount} IDRISS
               </p>
             ) : (
-              <Spinner className="size-4" />
+              <p className="text-label3 text-neutralGreen-700">0 IDRISS</p>
             )}
           </div>
         </div>
