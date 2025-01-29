bcd7fee9505de14886306704fd4dc551a5c1a751
macG <80455577+21142@users.noreply.github.com>
2025-01-29 20:31:03 +0100

    feat: implemented staked-events api endpoint (#372)

--- apps/main-landing/src/app/vault/constants.ts
+++ apps/main-landing/src/app/vault/constants.ts
@@ -152,3 +152,9 @@ export const CLAIMED_EVENT =
 
 export const CLAIM_CONTRACT_ADDRESS =
   '0xfD76c3a2A4534D815c9C5127119e6ea738A28837';
+
+export const STAKED_EVENT =
+  'event Staked(address indexed user, uint256 amount)';
+
+export const WITHDRAWN_EVENT =
+  'event Withdrawn(address indexed user, uint256 amount)';
