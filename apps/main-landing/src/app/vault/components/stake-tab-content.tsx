1e53e4c55f4186c529a5437f8c48ce7c601320f8
lennardevertz <levertz@idriss.xyz>
2025-01-29 22:46:25 +0100

    - lint

--- apps/main-landing/src/app/vault/components/stake-tab-content.tsx
+++ apps/main-landing/src/app/vault/components/stake-tab-content.tsx
@@ -30,10 +30,11 @@ import { TOKEN_TERMS_AND_CONDITIONS_LINK } from '@idriss-xyz/constants';
 import { ERC20_ABI } from '@/app/creators/donate/constants';
 import { GeoConditionalButton } from '@/components/token-section/components/geo-conditional-button';
 import { TxLoadingModal } from '@/app/claim/components/tx-loading-modal/tx-loading-modal';
-import { IDRISS_TOKEN_ADDRESS } from '@/components/token-section/constants';
 import { formatNumber } from '@/app/claim/components/claim/components/idriss-user-criteria-description';
 
 import { StakingABI, STAKER_ADDRESS } from '../constants';
+
+import { IDRISS_TOKEN_ADDRESS } from '@/components/token-section/constants';
 
 type FormPayload = {
   amount: number;
