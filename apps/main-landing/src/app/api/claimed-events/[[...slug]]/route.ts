5decd462752715ce2958ac06be25dac43c6c7022
lennardevertz <levertz@idriss.xyz>
2025-02-05 22:44:25 +0100

    unused exports

--- apps/main-landing/src/app/api/claimed-events/[[...slug]]/route.ts
+++ apps/main-landing/src/app/api/claimed-events/[[...slug]]/route.ts
@@ -103,6 +103,7 @@ const saveEventsToFile = (events: ClaimEvent[], lastProcessedBlock: bigint) => {
   fs.writeFileSync(DATA_FILE_PATH, JSON.stringify(data, null, 2), 'utf8');
 };
 
+// ts-unused-exports:disable-next-line
 export async function GET(
   request: Request,
 ): Promise<NextResponse<ApiResponse>> {
