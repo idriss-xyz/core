3e813d7d878188b7f7919d8216e110487ce4bf5d
Mateusz Stawski <mateuszmstawski@gmail.com>
2025-02-14 18:42:36 +0100

    fix: revert TipHistoryQuery amount per page

--- apps/main-landing/src/app/api/tip-history/constants.ts
+++ apps/main-landing/src/app/api/tip-history/constants.ts
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 export const TipHistoryQuery = gql`
   query ($addresses: [Address!], $isSigner: Boolean) {
-    accountsTimeline(addresses: $addresses, isSigner: $isSigner, first: 100) {
+    accountsTimeline(addresses: $addresses, isSigner: $isSigner, first: 20) {
       edges {
         node {
           timestamp
