ee6221e1b27b69b74d8a4e49685b3130c6def895
Mateusz Stawski <mateuszmstawski@gmail.com>
2025-03-05 12:32:21 +0100

    feat: add loader to user history list

--- apps/main-landing/src/app/creators/donate/components/user-history/user-history-list.tsx
+++ apps/main-landing/src/app/creators/donate/components/user-history/user-history-list.tsx
@@ -4,6 +4,7 @@ import { Link } from '@idriss-xyz/ui/link';
 import { Icon } from '@idriss-xyz/ui/icon';
 import { Hex } from 'viem';
 import { getShortWalletHex } from '@idriss-xyz/utils';
+import { Spinner } from '@idriss-xyz/ui/spinner';
 
 import { useGetDonorHistory } from '@/app/creators/donate/commands/get-donor-history';
 import { FilteredTopDonors } from '@/app/creators/donate/top-donors';
@@ -45,7 +46,7 @@ export default function UserHistoryList({
     { enabled: !!ensNameQuery.data },
   );
 
-  if (!donorHistory.data || !userDetails) {
+  if ((!donorHistory.isLoading && !donorHistory.data) || !userDetails) {
     if (backTo) {
       updateCurrentContent({ name: backTo });
     } else {
@@ -55,13 +56,8 @@ export default function UserHistoryList({
     return;
   }
 
-  console.log(donorHistory.data);
-
-  const stats = donorHistory.data.stats;
-  const leaderboard = donorHistory.data.leaderboard;
-
-  console.log(stats);
-  console.log(leaderboard);
+  const stats = donorHistory.data?.stats;
+  const leaderboard = donorHistory.data?.leaderboard;
 
   return (
     <div className="grid grid-cols-1 items-start gap-x-10 lg:grid-cols-[1fr,auto]">
@@ -75,104 +71,119 @@ export default function UserHistoryList({
 
         <h1 className="self-start text-heading4 text-neutralGreen-900">
           Donation stats of{' '}
-          {stats.donorDisplayName ?? userDetails.displayName.value}
+          {stats?.donorDisplayName ?? userDetails.displayName.value}
         </h1>
 
-        <div className="relative z-0 mt-9 grid w-full grid-cols-1 gap-3 lg:grid-cols-2">
-          <div className="flex flex-col items-center justify-center gap-y-2 rounded-2xl bg-white p-8 shadow-md">
-            <p className="text-label5 text-neutral-600">Donations</p>
-            <p className="text-heading4 text-neutral-800">
-              {stats.totalDonationsCount}
-            </p>
-          </div>
-          <div className="flex flex-col items-center justify-center gap-y-2 rounded-2xl bg-white p-8 shadow-md">
-            <p className="text-label5 text-neutral-600">Total volume</p>
-            <p className="text-heading4 text-neutral-800">
-              $
-              {new Intl.NumberFormat('en-US', {
-                minimumFractionDigits:
-                  stats.totalDonationAmount % 1 === 0 ? 0 : 2,
-                maximumFractionDigits: 2,
-              }).format(Number(stats.totalDonationAmount))}
-            </p>
-          </div>
-          <div className="flex flex-col items-center justify-center gap-y-2 rounded-2xl bg-white p-8 shadow-md">
-            <p className="text-label5 text-neutral-600">Largest donation</p>
-            <p className="text-heading4 text-neutral-800">
-              $
-              {new Intl.NumberFormat('en-US', {
-                minimumFractionDigits:
-                  stats.biggestDonationAmount % 1 === 0 ? 0 : 2,
-                maximumFractionDigits: 2,
-              }).format(Number(stats.biggestDonationAmount))}
-            </p>
-          </div>
-          {stats.favoriteTokenMetadata && (
-            <div className="flex flex-col items-center justify-center gap-y-2 rounded-2xl bg-white p-8 shadow-md">
-              <p className="text-label5 text-neutral-600">Favorite token</p>
-              <p className="text-heading4 text-neutral-800">
-                {stats.favoriteDonationToken}{' '}
-                <img
-                  className="inline-block size-6 rounded-full"
-                  src={stats.favoriteTokenMetadata.imageUrlV2}
-                  alt=""
-                />
-              </p>
-            </div>
-          )}
-          {stats.mostDonatedToAddress !== '' && (
-            <div className="flex flex-col items-center justify-center gap-y-2 rounded-2xl bg-white p-8 shadow-md">
-              <p className="text-label5 text-neutral-600">Top recipient</p>
-              <div className="flex flex-row items-center gap-x-1">
-                {ensAvatarQuery.data ? (
-                  <img
-                    className="size-10 rounded-full border border-neutral-400"
-                    src={ensAvatarQuery.data}
-                    alt="Donor avatar"
-                  />
-                ) : (
-                  <div className="flex size-10 items-center justify-center rounded-full border border-neutral-300 bg-neutral-200">
-                    <Icon
-                      size={25}
-                      name="CircleUserRound"
-                      className="text-neutral-500"
-                    />
-                  </div>
-                )}
-                <p className="text-label3 text-neutral-800">
-                  {ensNameQuery.data ??
-                    getShortWalletHex(stats.mostDonatedToAddress)}
+        {donorHistory.isLoading ? (
+          <Spinner className="mx-auto mt-9 size-16 text-mint-600" />
+        ) : stats ? (
+          <>
+            <div className="relative z-0 mt-9 grid w-full grid-cols-1 gap-3 lg:grid-cols-2">
+              <div className="flex flex-col items-center justify-center gap-y-2 rounded-2xl bg-white p-8 shadow-md">
+                <p className="text-label5 text-neutral-600">Donations</p>
+                <p className="text-heading4 text-neutral-800">
+                  {stats.totalDonationsCount}
                 </p>
               </div>
+              <div className="flex flex-col items-center justify-center gap-y-2 rounded-2xl bg-white p-8 shadow-md">
+                <p className="text-label5 text-neutral-600">Total volume</p>
+                <p className="text-heading4 text-neutral-800">
+                  $
+                  {new Intl.NumberFormat('en-US', {
+                    minimumFractionDigits:
+                      stats.totalDonationAmount % 1 === 0 ? 0 : 2,
+                    maximumFractionDigits: 2,
+                  }).format(Number(stats.totalDonationAmount))}
+                </p>
+              </div>
+              <div className="flex flex-col items-center justify-center gap-y-2 rounded-2xl bg-white p-8 shadow-md">
+                <p className="text-label5 text-neutral-600">Largest donation</p>
+                <p className="text-heading4 text-neutral-800">
+                  $
+                  {new Intl.NumberFormat('en-US', {
+                    minimumFractionDigits:
+                      stats.biggestDonationAmount % 1 === 0 ? 0 : 2,
+                    maximumFractionDigits: 2,
+                  }).format(Number(stats.biggestDonationAmount))}
+                </p>
+              </div>
+              {stats.favoriteTokenMetadata && (
+                <div className="flex flex-col items-center justify-center gap-y-2 rounded-2xl bg-white p-8 shadow-md">
+                  <p className="text-label5 text-neutral-600">Favorite token</p>
+                  <p className="text-heading4 text-neutral-800">
+                    {stats.favoriteDonationToken}{' '}
+                    <img
+                      className="inline-block size-6 rounded-full"
+                      src={stats.favoriteTokenMetadata.imageUrlV2}
+                      alt=""
+                    />
+                  </p>
+                </div>
+              )}
+              {stats.mostDonatedToAddress !== '' && (
+                <div className="flex flex-col items-center justify-center gap-y-2 rounded-2xl bg-white p-8 shadow-md">
+                  <p className="text-label5 text-neutral-600">Top recipient</p>
+                  <div className="flex flex-row items-center gap-x-1">
+                    {ensAvatarQuery.data ? (
+                      <img
+                        className="size-10 rounded-full border border-neutral-400"
+                        src={ensAvatarQuery.data}
+                        alt="Donor avatar"
+                      />
+                    ) : (
+                      <div className="flex size-10 items-center justify-center rounded-full border border-neutral-300 bg-neutral-200">
+                        <Icon
+                          size={25}
+                          name="CircleUserRound"
+                          className="text-neutral-500"
+                        />
+                      </div>
+                    )}
+                    <p className="text-label3 text-neutral-800">
+                      {ensNameQuery.data ??
+                        getShortWalletHex(stats.mostDonatedToAddress)}
+                    </p>
+                  </div>
+                </div>
+              )}
+              {stats.positionInLeaderboard && (
+                <div className="flex flex-col items-center justify-center gap-y-2 rounded-2xl bg-white p-8 shadow-md">
+                  <p className="text-label5 text-neutral-600">
+                    Leaderboard rank
+                  </p>
+                  <p className="text-heading4 text-neutral-800">
+                    #{stats.positionInLeaderboard}
+                  </p>
+                </div>
+              )}
             </div>
-          )}
-          {stats.positionInLeaderboard && (
-            <div className="flex flex-col items-center justify-center gap-y-2 rounded-2xl bg-white p-8 shadow-md">
-              <p className="text-label5 text-neutral-600">Leaderboard rank</p>
-              <p className="text-heading4 text-neutral-800">
-                #{stats.positionInLeaderboard}
-              </p>
-            </div>
-          )}
-        </div>
 
-        <div className="mt-12 flex justify-center">
-          <Link
-            size="xs"
-            onClick={() => {
-              updateCurrentContent({ name: 'history' });
-            }}
-            className="cursor-pointer"
-          >
-            See full donation history
-          </Link>
-        </div>
+            <div className="mt-12 flex justify-center">
+              <Link
+                size="xs"
+                onClick={() => {
+                  updateCurrentContent({ name: 'history' });
+                }}
+                className="cursor-pointer"
+              >
+                See full donation history
+              </Link>
+            </div>
+          </>
+        ) : (
+          <div className="mx-auto mt-9">
+            <p className="flex items-center justify-center gap-2 text-center text-heading4 text-red-500">
+              <Icon name="AlertCircle" size={40} />{' '}
+              <span>Cannot get donation stats</span>
+            </p>
+          </div>
+        )}
       </div>
 
       <FilteredTopDonors
-        leaderboard={leaderboard}
-        leaderboardLoading={donorHistory.isLoading}
+        leaderboard={leaderboard ?? []}
         leaderboardError={donorHistory.isError}
+        leaderboardLoading={donorHistory.isLoading}
         updateCurrentContent={updateCurrentContent}
         className="container mt-8 w-[360px] max-w-full overflow-hidden px-0 lg:mt-[130px] lg:[@media(max-height:800px)]:mt-[60px]"
       />
