4b7e7c59527f22b71da11268c010886a84824d8d
Daniel Arroyo (danielarroyo.eth) <34529009+daniel0ar@users.noreply.github.com>
2025-03-06 13:32:54 -0500

    refactor: Cleaner code on siwe login hook (#436)

--- apps/extension/src/application/trading-copilot/hooks/use-login-via-siwe.ts
+++ apps/extension/src/application/trading-copilot/hooks/use-login-via-siwe.ts
@@ -10,7 +10,6 @@ import {
   VerifySiweSignatureCommand,
   VerifyTokenCommand,
 } from '../commands';
-import { SiweMessageRequest, VerifySiweSignatureRequest } from '../types';
 
 export const useLoginViaSiwe = () => {
   const { setWalletInfo } = useWallet();
@@ -24,21 +23,9 @@ export const useLoginViaSiwe = () => {
       if (!wallet) {
         return;
       }
-
-      const handleGetSiweMessage = async (payload: SiweMessageRequest) => {
-        return await getSiweMessage.mutateAsync(payload);
-      };
-
-      const handleVerifySiweSignature = async (
-        payload: VerifySiweSignatureRequest,
-      ) => {
-        return await verifySiweSignature.mutateAsync(payload);
-      };
-
       const provider = wallet.provider;
-      const providerRdns = wallet.providerRdns;
 
-      const accounts = await provider.request({
+      const accounts = await wallet.provider.request({
         method: 'eth_requestAccounts',
       });
 
@@ -53,7 +40,7 @@ export const useLoginViaSiwe = () => {
           account: getAddress(accounts[0]),
           provider,
           chainId: hexToNumber(chainId),
-          providerRdns: providerRdns,
+          providerRdns: wallet.providerRdns,
         });
       } else {
         return;
@@ -61,24 +48,28 @@ export const useLoginViaSiwe = () => {
 
       const walletClient = createWalletClient(wallet);
 
-      const siweMessage = await handleGetSiweMessage({
+      const siweMessage = await getSiweMessage.mutateAsync({
         walletAddress: wallet.account,
         chainId: wallet.chainId,
         domain: window.location.hostname,
       });
 
-      const siweSignature = await walletClient.signMessage({
-        account: wallet.account,
-        message: siweMessage.message,
-      });
+      try {
+        const siweSignature = await walletClient.signMessage({
+          account: wallet.account,
+          message: siweMessage.message,
+        });
 
-      const verifiedSiweSignature = await handleVerifySiweSignature({
-        walletAddress: wallet.account,
-        message: siweMessage.message,
-        signature: siweSignature,
-      });
-
-      saveAuthToken(verifiedSiweSignature.token);
+        const verifiedSiweSignature = await verifySiweSignature.mutateAsync({
+          walletAddress: wallet.account,
+          message: siweMessage.message,
+          signature: siweSignature,
+        });
+        saveAuthToken(verifiedSiweSignature.token);
+      } catch (error) {
+        console.error('Error signing in with wallet.', error);
+        return;
+      }
     },
     [getSiweMessage, saveAuthToken, setWalletInfo, verifySiweSignature],
   );
@@ -95,13 +86,13 @@ export const useLoginViaSiwe = () => {
     });
   }, [getAuthToken, verifyAuthTokenMutation]);
 
-  const isSending = getSiweMessage.isPending || verifySiweSignature.isPending;
+  const isPending = getSiweMessage.isPending || verifySiweSignature.isPending;
 
   const isError = getSiweMessage.isError || verifySiweSignature.isError;
 
   const isSuccess = getSiweMessage.isSuccess && verifySiweSignature.isSuccess;
 
-  const isIdle = !isSending && !isError && !isSuccess;
+  const isIdle = !isPending && !isError && !isSuccess;
 
   const reset = useCallback(() => {
     getSiweMessage.reset();
@@ -111,7 +102,7 @@ export const useLoginViaSiwe = () => {
   return {
     login,
     loggedIn,
-    isSending,
+    isPending,
     isError,
     isSuccess,
     isIdle,
