6a2cd72bb159c972166a5b8bcc90beb3685f36c4
lennardevertz <levertz@idriss.xyz>
2025-03-13 17:33:50 +0100

    chore: Add whitelist comment for avatar image URLs in get-avatar-image.ts
    feat: Implement URL whitelist for avatar image fetching logic
    
    - add whitelisted urls
    
    dd whitelist for url fetching

--- apps/main-landing/src/app/creators/donate/commands/get-avatar-image.ts
+++ apps/main-landing/src/app/creators/donate/commands/get-avatar-image.ts
@@ -8,13 +8,28 @@ type Options = {
   enabled?: boolean;
 };
 
-const getAvatarImage = async (payload: Payload) => {
+const WHITELISTED_URLS = [
+  'https://imagedelivery.net/',
+  'https://i.imgur.com/',
+  'https://ik.imagekit.io/',
+  'https://euc.li/',
+];
+
+const getAvatarImage = async (payload: Payload): Promise<string> => {
+  // Check if the URL is in the whitelist
+  const isWhitelisted = WHITELISTED_URLS.some((baseUrl) => {
+    return payload.url.startsWith(baseUrl);
+  });
+  if (isWhitelisted) {
+    return payload.url;
+  }
   const response = await fetch(
     `https://api.idriss.xyz/fetch-image?url=${payload.url}`,
   );
-
+  if (!response.ok) {
+    throw new Error('Failed to fetch avatar image');
+  }
   const avatarImage = await response.json();
-
   return avatarImage.image as string;
 };
 
