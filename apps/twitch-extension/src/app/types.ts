ba64d6933d32942815f7e2ce68281c95f033b36d
Mateusz Stawski <100126546+MateuszStawski@users.noreply.github.com>
2025-03-13 21:23:41 +0100

    feat: add views for twitch extension (#423)
    Co-authored-by: lennardevertz <levertz@idriss.xyz>

---
+++ apps/twitch-extension/src/app/types.ts
@@ -0,0 +1,12 @@
+export type FormValues = {
+  donationLink: string;
+};
+
+export type ConfigValues = {
+  donationLink: string;
+  address: string;
+};
+
+export type WidgetVariants = 'panel' | 'videoOverlay' | 'videoComponent';
+
+export type ConfigSubmitStatus = 'error' | 'success';
