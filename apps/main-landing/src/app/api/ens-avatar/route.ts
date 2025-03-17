f7ef16065494f6fa78740382d6581df5a48489bd
Mateusz Stawski <100126546+MateuszStawski@users.noreply.github.com>
2025-03-17 17:17:47 +0100

    fix: removed cors error in ens avatar endpoint (#459)

--- apps/main-landing/src/app/api/ens-avatar/route.ts
+++ apps/main-landing/src/app/api/ens-avatar/route.ts
@@ -3,6 +3,10 @@ import { createPublicClient, http } from 'viem';
 import { mainnet } from 'viem/chains';
 import { normalize } from 'viem/ens';
 
+const DEFAULT_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+};
+
 const client = createPublicClient({
   chain: mainnet,
   transport: http('https://eth.llamarpc.com'),
@@ -13,19 +17,29 @@ export async function GET(request: Request) {
   const ensDomain = url.searchParams.get('ens');
 
   if (!ensDomain) {
-    return NextResponse.json({ error: 'Domain not provided' }, { status: 400 });
+    return NextResponse.json(
+      { error: 'Domain not provided' },
+      {
+        status: 400,
+        headers: DEFAULT_HEADERS,
+      },
+    );
   }
 
   try {
     const avatar_url = await client.getEnsAvatar({
       name: normalize(ensDomain),
     });
-    return NextResponse.json({ image: avatar_url }, { status: 200 });
+    return NextResponse.json(
+      { image: avatar_url },
+      { status: 200, headers: DEFAULT_HEADERS },
+    );
   } catch (error) {
     console.error('[ERROR] Failed fetching or saving stake events:', error);
+
     return NextResponse.json(
       { error: 'Failed to fetch events' },
-      { status: 500 },
+      { status: 500, headers: DEFAULT_HEADERS },
     );
   }
 }
