7488faf66361fb9b2d9c534dcd01d3cbb4ace255
Levertz <95511599+lennardevertz@users.noreply.github.com>
2025-03-17 17:47:29 +0100

    Revert "- stop fetching claiming events (#452)"
    This reverts commit a50c2bf79846b5bd01e9656a1ad46c498323cbf4.

--- apps/main-landing/src/app/vault/constants.ts
+++ apps/main-landing/src/app/vault/constants.ts
@@ -1,3 +1,9 @@
+export const CLAIMED_EVENT =
+  'event Claimed(address by, address token, address to, uint256 total, uint256[] claimIndices, bytes memo, bool bonus)';
+
+export const CLAIM_CONTRACT_ADDRESS =
+  '0xfD76c3a2A4534D815c9C5127119e6ea738A28837';
+
 export const STAKED_EVENT =
   'event Staked(address indexed user, uint256 amount)';
 
