64c01eb61d7bc60e1002e10271f9000cff00c6ff
Levertz <95511599+lennardevertz@users.noreply.github.com>
2025-03-17 16:43:05 +0100

    - minor api change (#458)

--- apps/main-landing/src/app/api/ens-avatar/route.ts
+++ apps/main-landing/src/app/api/ens-avatar/route.ts
@@ -1,37 +1,31 @@
-import { createPublicClient, GetEnsAvatarReturnType, http } from 'viem';
-import { mainnet } from 'viem/chains';
 import { NextResponse } from 'next/server';
+import { createPublicClient, http } from 'viem';
+import { mainnet } from 'viem/chains';
 import { normalize } from 'viem/ens';
-
-interface ApiResponse {
-  error?: string;
-  image?: GetEnsAvatarReturnType;
-}
 
 const client = createPublicClient({
   chain: mainnet,
   transport: http('https://eth.llamarpc.com'),
 });
-
 // ts-unused-exports:disable-next-line
-export async function GET(
-  request: Request,
-): Promise<NextResponse<ApiResponse>> {
+export async function GET(request: Request) {
   const url = new URL(request.url);
   const ensDomain = url.searchParams.get('ens');
-  if (!ensDomain)
-    return NextResponse.json({ error: 'Domain not provided', status: 400 });
+
+  if (!ensDomain) {
+    return NextResponse.json({ error: 'Domain not provided' }, { status: 400 });
+  }
+
   try {
     const avatar_url = await client.getEnsAvatar({
       name: normalize(ensDomain),
     });
-
-    return NextResponse.json({ image: avatar_url });
+    return NextResponse.json({ image: avatar_url }, { status: 200 });
   } catch (error) {
     console.error('[ERROR] Failed fetching or saving stake events:', error);
     return NextResponse.json(
       { error: 'Failed to fetch events' },
-      { status: 400 },
+      { status: 500 },
     );
   }
 }
