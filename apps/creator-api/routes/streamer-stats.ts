f192a8db21746b736896e21e3afc7bb6de9dcbee
Levertz <95511599+lennardevertz@users.noreply.github.com>
2025-03-18 14:10:11 +0100

    - dont require address on streamer stats page (#463)

--- apps/creator-api/routes/streamer-stats.ts
+++ apps/creator-api/routes/streamer-stats.ts
@@ -1,11 +1,9 @@
 import { Router, Request, Response } from 'express';
 
-import { fetchDonationsByToAddress } from '../db/fetch-known-donations';
 import { calculateGlobalStreamerLeaderboard } from '../utils/calculate-stats';
 import dotenv from 'dotenv';
 import { mode } from '../utils/mode';
 import { join } from 'path';
-import { Hex } from 'viem';
 
 const router = Router();
 
@@ -15,13 +13,6 @@ dotenv.config(
 
 router.post('/', async (req: Request, res: Response) => {
   try {
-    const { address } = req.body;
-
-    if (!address || typeof address !== 'string') {
-      res.status(400).json({ error: 'Invalid or missing address' });
-      return;
-    }
-
     const leaderboard = await calculateGlobalStreamerLeaderboard();
 
     res.json({
