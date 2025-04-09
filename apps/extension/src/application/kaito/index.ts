ece80adc27e63b44b6defdde1e7a5dbd5efb512b
Mateusz Stawski <mateuszmstawski@gmail.com>
2025-04-09 12:18:37 +0200

    fix: remove unused exports

--- apps/extension/src/application/kaito/index.ts
+++ apps/extension/src/application/kaito/index.ts
@@ -1,4 +1,3 @@
 export { GetYapsCommand, GetSmartFollowersCommand } from './commands';
-export type { YapResponse, SmartFollowersResponse } from './types';
 export { COMMAND_MAP as KAITO_COMMAND_MAP } from './commands';
 export { checkForOrganizationBadge } from './utils';
