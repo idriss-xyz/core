e2da0d312e60091492476ff7313f8f79281839db
Daniel <daniel0ar@hotmail.com>
2025-04-15 12:36:12 -0500

    style(creators): Format text-to-speech.ts

--- apps/creator-api/routes/text-to-speech.ts
+++ apps/creator-api/routes/text-to-speech.ts
@@ -25,40 +25,48 @@ const requestLimitation = rateLimit({
   legacyHeaders: false,
 });
 
-router.post('/', requestLimitation, validationRules, async (req: Request, res: Response) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(400).json({ errors: errors.array() });
-    return;
-  }
-  try {
-    const { text } = req.body;
-    const trimmedText = text.length > 70 ? text.slice(0, 70) : text;
-
-    const response = await fetch(
-      `https://api.elevenlabs.io/v1/text-to-speech/${ELEVENLABS_VOICE_ID}`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'xi-api-key': `${ELEVENLABS_API_KEY}`,
-        },
-        body: JSON.stringify({ text: trimmedText, model_id: 'eleven_multilingual_v2' }),
-      },
-    );
-
-    if (!response.body) {
-      res.status(500).json({ error: 'Failed to request text to speech' });
+router.post(
+  '/',
+  requestLimitation,
+  validationRules,
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
       return;
     }
+    try {
+      const { text } = req.body;
+      const trimmedText = text.length > 70 ? text.slice(0, 70) : text;
 
-    res.setHeader('Content-Type', 'audio/mpeg');
-    const nodeStream = Readable.fromWeb(response.body as any);
-    nodeStream.pipe(res);
-  } catch (error) {
-    console.error('Text to speech error: ', error);
-    res.status(500).json({ error: 'Failed to request text to speech' });
-  }
-});
+      const response = await fetch(
+        `https://api.elevenlabs.io/v1/text-to-speech/${ELEVENLABS_VOICE_ID}`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'xi-api-key': `${ELEVENLABS_API_KEY}`,
+          },
+          body: JSON.stringify({
+            text: trimmedText,
+            model_id: 'eleven_multilingual_v2',
+          }),
+        },
+      );
+
+      if (!response.body) {
+        res.status(500).json({ error: 'Failed to request text to speech' });
+        return;
+      }
+
+      res.setHeader('Content-Type', 'audio/mpeg');
+      const nodeStream = Readable.fromWeb(response.body as any);
+      nodeStream.pipe(res);
+    } catch (error) {
+      console.error('Text to speech error: ', error);
+      res.status(500).json({ error: 'Failed to request text to speech' });
+    }
+  },
+);
 
 export default router;
