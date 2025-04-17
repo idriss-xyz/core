dc4e10623c9d31144295bbaec9d11a3a2b250f9a
Daniel Arroyo <daniel0ar@hotmail.com>
2025-04-17 09:09:11 -0500

    fix(creators): Trim text before checking length

--- apps/creator-api/routes/text-to-speech.ts
+++ apps/creator-api/routes/text-to-speech.ts
@@ -38,7 +38,8 @@ router.post(
     }
     try {
       const { text } = req.body;
-      const trimmedText = text.length > 70 ? text.trim().slice(0, 70) : text;
+      const trimmedText = text.trim();
+      const finalText = trimmedText.length > 70 ? trimmedText.slice(0, 70) : trimmedText;
 
       const response = await fetch(
         `https://api.elevenlabs.io/v1/text-to-speech/${ELEVENLABS_VOICE_ID}`,
@@ -49,7 +50,7 @@ router.post(
             'xi-api-key': `${ELEVENLABS_API_KEY}`,
           },
           body: JSON.stringify({
-            text: trimmedText,
+            text: finalText,
             model_id: 'eleven_multilingual_v2',
           }),
         },
