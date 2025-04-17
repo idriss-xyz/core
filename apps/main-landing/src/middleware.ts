204b67c5efd72de1b14bcd37692370b47663f4eb
lennardevertz <levertz@idriss.xyz>
2025-04-17 11:54:28 +0200

    add rpc to baseclient

--- apps/main-landing/src/middleware.ts
+++ apps/main-landing/src/middleware.ts
@@ -5,13 +5,13 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const url = request.nextUrl;
   const password = request.cookies.get('password')?.value;
+  const response = NextResponse.next();
 
-  if (process.env.PUBLIC_ACCESS_ENABLED === 'true') {
-    return NextResponse.next();
-  }
+  response.headers.set('X-Frame-Options', 'DENY');
 
   if (
     ['/vault', '/claim'].includes(url.pathname) &&
+    process.env.PUBLIC_ACCESS_ENABLED !== 'true' &&
     (password !== process.env.DEV_LOGIN_PASSWORD ||
       !process.env.DEV_LOGIN_PASSWORD)
   ) {
@@ -20,10 +20,9 @@ export function middleware(request: NextRequest) {
     return NextResponse.redirect(loginUrl);
   }
 
-  return NextResponse.next();
+  return response;
 }
 
-// ts-unused-exports:disable-next-line
 export const config = {
-  matcher: ['/vault', '/claim'],
+  matcher: '/:path*',
 };
