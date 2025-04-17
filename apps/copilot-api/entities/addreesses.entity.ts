cb2edf3cb831f0fdbfd51d3b1d11826432da21cb
Levertz <95511599+lennardevertz@users.noreply.github.com>
2025-04-17 12:22:20 +0200

    fix imports of migrations (#512)

--- apps/copilot-api/entities/addreesses.entity.ts
+++
@@ -1,7 +0,0 @@
-import { Entity, PrimaryColumn } from 'typeorm';
-
-@Entity('addresses')
-export class AddressesEntity {
-  @PrimaryColumn({ type: 'text', name: 'address' })
-  readonly address!: string;
-}
