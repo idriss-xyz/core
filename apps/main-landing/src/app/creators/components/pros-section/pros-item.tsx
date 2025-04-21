89709646c31d1d4f7643665b8b4fbd8ca8ce06f1
Daniel <daniel0ar@hotmail.com>
2025-04-21 14:18:05 -0500

    fix: Description size on ProsItem

--- apps/main-landing/src/app/creators/components/pros-section/pros-item.tsx
+++ apps/main-landing/src/app/creators/components/pros-section/pros-item.tsx
@@ -21,29 +21,31 @@ export const ProsItem = ({
     <div
       className={classes('flex w-full flex-col items-start gap-y-2', className)}
     >
-      <Icon
-        size={32}
-        name={iconName}
-        className="size-6 text-mint-400 md:size-8"
-      />
+      <div className='max-w-[510px] flex flex-col gap-3'>
+        <Icon
+          size={32}
+          name={iconName}
+          className="size-6 text-mint-400 md:size-8"
+        />
 
-      <h3
-        className={classes(
-          'max-w-[500px] text-heading4 text-midnightGreen-100',
-          'md:text-heading3',
-          'lg:text-heading2',
-        )}
-      >
-        {heading}
-      </h3>
+        <h3
+          className={classes(
+            'text-heading4 text-midnightGreen-100',
+            'md:text-heading3',
+            'lg:text-heading2',
+          )}
+        >
+          {heading}
+        </h3>
 
-      <p
-        className={classes(
-          'max-w-[510px] text-body4 text-midnightGreen-200 lg:text-balance',
-        )}
-      >
-        {description}
-      </p>
+        <p
+          className={classes(
+            'text-body4 text-midnightGreen-200 lg:text-wrap',
+          )}
+        >
+          {description}
+        </p>
+      </div>
 
       {children}
     </div>
