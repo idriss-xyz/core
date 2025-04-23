e17a6b189a12ddef036b41a1538e083d0ac93238
Daniel Arroyo (danielarroyo.eth) <34529009+daniel0ar@users.noreply.github.com>
2025-04-23 07:58:53 -0500

    feat: Add Pandapple to creators (#524)

--- apps/main-landing/src/app/creators/components/top-creators-section/top-creators-showcase.tsx
+++ apps/main-landing/src/app/creators/components/top-creators-section/top-creators-showcase.tsx
@@ -14,6 +14,7 @@ import {
   ViperCard,
   YoJurmyCard,
   Alliestrasza,
+  Pandapple,
 } from './assets';
 
 const creators = [
@@ -135,6 +136,24 @@ const creators = [
           asLink
           isExternal
           href="https://www.twitch.tv/lambyseriestv"
+          iconName="TwitchOutlinedBold"
+          size="extra"
+          intent="tertiary"
+          className="size-6 text-mint-200 md:size-8"
+        />
+      ),
+    },
+  },
+  {
+    name: 'Pandapple',
+    followers: '5.5K',
+    image: Pandapple.src,
+    platform: {
+      icon: (
+        <IconButton
+          asLink
+          isExternal
+          href="https://www.twitch.tv/pandapple"
           iconName="TwitchOutlinedBold"
           size="extra"
           intent="tertiary"
