a40f52db661d30bb0065ad495ef976c24c55f117
Daniel <daniel0ar@hotmail.com>
2025-04-28 15:05:51 -0500

    feat: Display and play alert only if donation > $1

--- apps/main-landing/src/app/creators/obs/hooks/use-donation-notification.ts
+++ apps/main-landing/src/app/creators/obs/hooks/use-donation-notification.ts
@@ -3,6 +3,7 @@ import { useState, useEffect, useRef } from 'react';
 import { getTextToSpeech } from '../utils';
 
 const DONATION_TTS_MIN_AMOUNT = 4.75; // $5 minus 5% margin for price drops
+const DONATION_ALERT_MIN_AMOUNT = 0.95; // $1 minus 5% margin for price drops
 const DONATION_TTS_DELAY = 2000;
 
 export const useDonationNotification = (
@@ -18,34 +19,34 @@ export const useDonationNotification = (
     if (!amount || hasRunReference.current) return;
 
     hasRunReference.current = true;
-    audio.play().catch((error) => {
-      console.error('Audio playback failed:', error);
-    });
-
-    if (Number.parseFloat(amount) > DONATION_TTS_MIN_AMOUNT) {
-      getTextToSpeech(message)
-        .then(async (stream) => {
-          if (stream) {
-            const arrayBuffer = await stream.arrayBuffer();
-            const blob = new Blob([arrayBuffer], { type: 'audio/mpeg' });
-            const audioUrl = URL.createObjectURL(blob);
-
-            const speech = new Audio(audioUrl);
-            setTimeout(() => {
-              speech.play().catch((error) => {
-                console.error('Audio playback failed:', error);
-              });
-            }, DONATION_TTS_DELAY);
-          } else {
-            throw new Error('Audio stream from api is null');
-          }
-        })
-        .catch((error) => {
-          console.error('Error fetching superdonation stream:', error);
-        });
-
+    if (Number.parseFloat(amount) > DONATION_ALERT_MIN_AMOUNT) {
       setShowNotification(true);
+      audio.play().catch((error) => {
+        console.error('Audio playback failed:', error);
+      });
 
+      if (Number.parseFloat(amount) > DONATION_TTS_MIN_AMOUNT) {
+        getTextToSpeech(message)
+          .then(async (stream) => {
+            if (stream) {
+              const arrayBuffer = await stream.arrayBuffer();
+              const blob = new Blob([arrayBuffer], { type: 'audio/mpeg' });
+              const audioUrl = URL.createObjectURL(blob);
+
+              const speech = new Audio(audioUrl);
+              setTimeout(() => {
+                speech.play().catch((error) => {
+                  console.error('Audio playback failed:', error);
+                });
+              }, DONATION_TTS_DELAY);
+            } else {
+              throw new Error('Audio stream from api is null');
+            }
+          })
+          .catch((error) => {
+            console.error('Error fetching superdonation stream:', error);
+          });
+      }
       const timeout = setTimeout(() => {
         setShowNotification(false);
         hasRunReference.current = false;
