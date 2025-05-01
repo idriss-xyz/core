8501374f2d71739830c1edba5cf47bd03b8b7b70
Daniel <daniel0ar@hotmail.com>
2025-05-01 13:43:43 -0500

    feat: Add backend entities for creator profile page

---
+++ apps/creator-api/db/entities/creator.entity.ts
@@ -0,0 +1,30 @@
+import { Column, Entity, PrimaryGeneratedColumn, ManyToMany } from 'typeorm';
+import { Hex } from 'viem';
+import { Network } from './network.entity';
+
+@Entity('creator')
+export class Creator {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'text', unique: true })
+  address!: Hex;
+
+  @Column({ type: 'text', name: 'primary_address', unique: true })
+  primaryAddress!: Hex;
+
+  @Column({ type: 'text' })
+  name!: Hex;
+
+  @Column({ type: 'text', name: 'profile_picture_url', nullable: true })
+  profilePictureUrl!: string;
+
+  @Column({ type: 'text', name: 'donation_url', nullable: true })
+  donationUrl!: string;
+
+  @Column({ type: 'text', name: 'obs_url', nullable: true })
+  obsUrl!: string;
+
+  @ManyToMany(() => Network, (network) => network.creators)
+  networks!: Network[];
+}
