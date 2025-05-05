4ea76686c4ab27930529f16dee558f8c560e475e
Daniel <daniel0ar@hotmail.com>
2025-05-05 08:55:10 -0500

    fet: Add creators/[name] page

---
+++ apps/main-landing/src/app/creators/[name]/page.tsx
@@ -0,0 +1,13 @@
+import { CreatorProfileForm } from './form';
+
+export default async function CreatorProfile({
+  params,
+}: {
+  params: Promise<{ name: string }>
+}) {
+  // You can use async/await here for server-side data fetching
+  const { name } = await params;
+
+  // Pass the name to the client component
+  return <CreatorProfileForm initialName={name} />;
+}
