1c03b40b55df74cf7662bcd34d778c77177ad872
Daniel <daniel0ar@hotmail.com>
2025-05-08 07:56:31 -0500

    fix: New donate form page mising styling

--- apps/main-landing/src/app/creators/[name]/page.tsx
+++ apps/main-landing/src/app/creators/[name]/page.tsx
@@ -1,5 +1,7 @@
+import { TopBar } from '@/components';
 import { Providers } from '../providers';
 import { DonateForm } from './donate-form';
+import { backgroundLines2 } from '@/assets';
 
 export default async function CreatorProfile({
   params,
@@ -12,7 +14,18 @@ export default async function CreatorProfile({
   // Pass the name to the client component
   return (
     <Providers>
-      <DonateForm initialName={name} />
+      <TopBar />
+        <main className="relative flex min-h-screen grow flex-col items-center justify-around gap-4 overflow-hidden bg-[radial-gradient(181.94%_192.93%_at_16.62%_0%,_#E7F5E7_0%,_#76C282_100%)] px-2 pb-1 pt-[56px] lg:flex-row lg:items-start lg:justify-center lg:px-0">
+          <link rel="preload" as="image" href={backgroundLines2.src} />
+          <img
+            alt=""
+            src={backgroundLines2.src}
+            className="pointer-events-none absolute top-0 hidden size-full opacity-40 lg:block"
+          />
+        <div className="grid grid-cols-1 items-start gap-x-10 lg:grid-cols-[1fr,auto]">
+          <DonateForm initialName={name} className="container mt-8 overflow-hidden lg:mt-[130px] lg:[@media(max-height:800px)]:mt-[60px]"/>
+        </div>
+      </main>
     </Providers>
   );
 }
