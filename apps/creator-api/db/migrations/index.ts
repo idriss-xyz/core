03f4008538c6507b36ce87c981efec1d75a9d73b
Daniel Arroyo (danielarroyo.eth) <34529009+daniel0ar@users.noreply.github.com>
2025-05-20 03:59:40 -0500

    Fix: Sfx donation 404s, temp storage issues and more (#540)
    Co-authored-by: lennardevertz <levertz@idriss.xyz>

--- apps/creator-api/db/migrations/index.ts
+++ apps/creator-api/db/migrations/index.ts
@@ -1,3 +1,4 @@
 export { RestructureDonations1743174000000 } from './1743174000000-RestructureDonations';
 export { AddTokenDecimals1743177600000 } from './1743177600000-AddTokenDecimals';
 export { AddAmountRaw1743181200000 } from './1743181200000-AddAmountRaw';
+export { AddDonationEffects1747686797772 } from './1747686797772-AddDonationEffects';
