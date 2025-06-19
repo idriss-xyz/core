0438f58a84cdce1d2fd3c42b5ceb43875c846e6f
Daniel <daniel0ar@hotmail.com>
2025-06-19 06:15:06 -0500

    fix: Error message appearing on "Save" creator profile button click

--- apps/main-landing/src/app/creators/app/form.tsx
+++ apps/main-landing/src/app/creators/app/form.tsx
@@ -340,7 +340,6 @@ export function CreatorProfileForm() {
 
   const onSubmit = async (data: FormPayload) => {
     setIsSaving(true);
-    setSaveSuccess(false);
 
     const chainsShortNames = getChainShortNamesFromIds(data.chainsIds);
 
