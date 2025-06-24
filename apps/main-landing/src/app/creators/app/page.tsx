4ea44fa8e6008ded83994280e1163ba2f6ba5150
Daniel <daniel0ar@hotmail.com>
2025-06-24 13:36:57 -0500

    feat: Add Transactions card stats badges

--- apps/main-landing/src/app/creators/app/page.tsx
+++ apps/main-landing/src/app/creators/app/page.tsx
@@ -1,6 +1,7 @@
 import { Card, CardHeader, CardBody } from '@idriss-xyz/ui/card';
 
 import { IDRISS_COIN } from '@/assets';
+import { Icon } from '@idriss-xyz/ui/icon';
 
 export default function Home() {
   return (
@@ -9,8 +10,22 @@ export default function Home() {
       <Card className="col-span-1">
         <CardHeader>Transactions</CardHeader>
         <CardBody>
-          <div className="flex items-center justify-center p-14">
-            <img src={IDRISS_COIN.src} alt="coin" />
+          <div className='relative'>
+            <div className="flex items-center justify-center mx-14 my-4 p-6">
+              <img src={IDRISS_COIN.src} alt="coin" />
+            </div>
+            <span className='flex justify-center items-center absolute px-2 py-1.5 font-[500] text-black bg-mint-200 rounded-full pointer-events-none top-16 left-6'>
+              <Icon size={24} name='BadgeDollarSign' className='mr-1 bg-mint-600 rounded-full text-white p-1' />
+              32 donations
+            </span>
+            <span className='flex justify-center items-center absolute px-2 py-1.5 font-[500] text-black bg-mint-200 rounded-full pointer-events-none top-8 right-8'>
+              <Icon size={24} name='Trophy' className='mr-1 bg-mint-600 rounded-full text-white p-1' />
+              5 donors
+            </span>
+            <span className='flex justify-center items-center absolute px-2 py-1.5 font-[500] text-black bg-mint-200 rounded-full pointer-events-none bottom-8 right-8'>
+              <Icon size={24} name='TrendingUp' className='mr-1 bg-mint-600 rounded-full text-white p-1' />
+              $100 largest donation
+            </span>
           </div>
         </CardBody>
       </Card>
