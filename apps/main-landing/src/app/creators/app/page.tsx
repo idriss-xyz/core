56c24ed5b2164ba1dcff86d14983c71b233ddbb1
Daniel <daniel0ar@hotmail.com>
2025-06-25 08:18:54 -0500

    feat: Add bar chart to "Total earnings" in dashbard (dummy)

--- apps/main-landing/src/app/creators/app/page.tsx
+++ apps/main-landing/src/app/creators/app/page.tsx
@@ -1,7 +1,30 @@
+'use client'
 import { Card, CardHeader, CardBody } from '@idriss-xyz/ui/card';
+import { ChartConfig, ChartContainer } from '@idriss-xyz/ui/charts';
+import { Bar, BarChart } from "recharts"
 
 import { IDRISS_COIN } from '@/assets';
 import { Icon } from '@idriss-xyz/ui/icon';
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#2563eb",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#60a5fa",
+  },
+} satisfies ChartConfig
 
 export default function Home() {
   return (
@@ -31,9 +54,18 @@ export default function Home() {
       </Card>
       <Card className="col-span-1">
         <CardHeader>Total earnings</CardHeader>
+        <CardBody>
+          <span className='text-heading3 text-black mb-2'>$920</span>
+          <ChartContainer config={chartConfig} className="min-h-[200px] w-full">
+            <BarChart accessibilityLayer data={chartData}>
+              <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+              <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+            </BarChart>
+          </ChartContainer>
+        </CardBody>
       </Card>
       <Card className="col-span-1">
-        <CardHeader>Earnings by token</CardHeader>
+        <CardHeader>Earnings by asset</CardHeader>
       </Card>
       <Card className="col-span-3">
         <CardBody>history</CardBody>
