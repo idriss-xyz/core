1c71237bb022dea64a0546817c4ede7c8c473fd6
lennardevertz <levertz@idriss.xyz>
2025-07-07 01:36:21 +0200

    rm unsued import as we need to import directly

--- apps/main-landing/src/app/vault/components/index.ts
+++ apps/main-landing/src/app/vault/components/index.ts
@@ -1,3 +1,2 @@
 export { StakeTabContent } from './stake-tab-content';
 export { UnstakeTabContent } from './unstake-tab-content';
-export { MobileNotSupportedContent } from './mobile-not-supported';
