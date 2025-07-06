d51cdf5aef2445e57db6f6ed0c5e189e113d135d
lennardevertz <levertz@idriss.xyz>
2025-07-06 21:49:54 +0200

    update address

--- packages/constants/src/constants.ts
+++ packages/constants/src/constants.ts
@@ -40,7 +40,7 @@ import {
 import { Chain, Token, ChainToken } from './types';
 
 export const STAKER_ADDRESS = '0x085e2DC1b05dcdbE011B5ad377C9f2fcD69B7057';
-export const REWARDS_ADDRESS = '0xD6b7C1903C56BAaD04aA1d4BAc21F5B612A261C7';
+export const REWARDS_ADDRESS = '0x4D66A8e9Da1F007802338B372aD348B78b455aBB';
 
 export const NATIVE_COIN_ADDRESS = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
 
