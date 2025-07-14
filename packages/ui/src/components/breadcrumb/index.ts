ef0a739b7cd2f2d443594faab47b9897c30cc4ba
Daniel <daniel0ar@hotmail.com>
2025-07-14 12:02:12 -0500

    feat: Add Breadcrumb component (shadcn + radix)

---
+++ packages/ui/src/components/breadcrumb/index.ts
@@ -0,0 +1,12 @@
+export {
+  Breadcrumb,
+  BreadcrumbList,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+  BreadcrumbEllipsis,
+} from './breadcrumb';
+
+export type { SiteMapItem } from './breadcrumb-navigation';
+export { BreadcrumbNavigation } from './breadcrumb-navigation';
