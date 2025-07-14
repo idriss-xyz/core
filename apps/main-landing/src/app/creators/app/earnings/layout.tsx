6dfdc674640f9e3bb3fca6404acf7b60dbeb8d4b
Daniel <daniel0ar@hotmail.com>
2025-07-14 12:07:00 -0500

    feat: Add breadcrumb to earnings page

--- apps/main-landing/src/app/creators/app/earnings/layout.tsx
+++ apps/main-landing/src/app/creators/app/earnings/layout.tsx
@@ -1,8 +1,34 @@
 'use client';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
-import { Icon } from '@idriss-xyz/ui/icon';
+import { BreadcrumbNavigation, SiteMapItem } from '@idriss-xyz/ui/breadcrumb';
 import { TabsPill, TabItem } from '@idriss-xyz/ui/tabs-pill';
+
+const siteMap: SiteMapItem[] = [
+  {
+    name: 'Home',
+    path: '/',
+  },
+  {
+    name: 'Earnings',
+    path: '/earnings',
+    children: [
+      {
+        name: 'Stats & history',
+        path: '/earnings/stats',
+      },
+      {
+        name: 'Balance',
+        path: '/earnings/balance',
+      },
+      {
+        name: 'Top donors',
+        path: '/earnings/top-donors',
+      },
+    ],
+  },
+  // TODO: Add more pages here and move to general layout
+];
 
 // ts-unused-exports:disable-next-line
 export default function EarningsLayout({
@@ -21,9 +47,11 @@ export default function EarningsLayout({
     },
     {
       name: 'Stats & history',
-      href: '/creators/app/earnings',
+      href: '/creators/app/earnings/stats',
       iconName: 'LineChart',
-      isActive: pathname === '/creators/app/earnings',
+      isActive:
+        pathname === '/creators/app/earnings/stats' ||
+        pathname === '/creators/app/earnings',
     },
     {
       name: 'Top donors',
@@ -47,31 +75,12 @@ export default function EarningsLayout({
   return (
     <div>
       <div className="mb-4 flex flex-col gap-4">
-        <div className="flex flex-col">
-          <div className="flex items-center gap-2">
-            <div className="flex gap-2.5">
-              <span className="max-h-[48px] min-h-4.5 min-w-[46px] max-w-[76px] text-body6 text-neutral-800">
-                Earnings
-              </span>
-            </div>
-            <div className="size-[16px] max-h-[46px] min-h-[16px] min-w-[16px] max-w-[46px]">
-              <Icon name="IdrissArrowRight" size={16} className="text-black" />
-            </div>
-
-            <div className="flex gap-2.5">
-              <span className="h-4.5 max-h-[48px] min-h-4.5 w-[79px] min-w-[79px] max-w-[109px] text-body6 text-neutral-800">
-                Stats & history
-              </span>
-            </div>
-          </div>
-
-          <div className="flex gap-2.5">
-            <div className="flex h-[40px] max-h-[70px] min-h-[40px] w-[213px] min-w-[213px] max-w-[243px] items-center">
-              <h1 className="col-span-3 my-4 text-heading3">Earnings</h1>
-            </div>
-          </div>
-        </div>
-
+        <BreadcrumbNavigation
+          pathname={pathname}
+          siteMap={siteMap}
+          basePath="/creators/app"
+        />
+        <h1 className="text-heading3">Earnings</h1>
         <TabsPill tabs={earningsTabs} renderLink={renderLink} />
       </div>
       {children}
