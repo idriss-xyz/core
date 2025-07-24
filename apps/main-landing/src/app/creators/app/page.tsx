e78786ff23f886bb5d8e8025f0f09fe234406294
Daniel <daniel0ar@hotmail.com>
2025-07-23 23:43:05 -0500

    feat: Add skeleton on v2 app load (for tests)

--- apps/main-landing/src/app/creators/app/page.tsx
+++ apps/main-landing/src/app/creators/app/page.tsx
@@ -4,8 +4,6 @@ import { useEffect } from 'react';
 
 import { useAuth } from '../context/auth-context';
 
-import Setup from './setup/page';
-
 // ts-unused-exports:disable-next-line
 export default function Home() {
   const { creator } = useAuth();
@@ -22,5 +20,30 @@ export default function Home() {
     }
   }, [creator, router]);
 
-  return <Setup />;
+  return (
+    <div
+      role="status"
+      className="size-full animate-pulse space-y-4 divide-y divide-gray-200 rounded-sm border border-gray-200 p-4 pt-5 shadow-sm md:p-6"
+    >
+      <div className="flex items-center justify-between">
+        <div>
+          <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-200" />
+          <div className="h-2 w-32 rounded-full bg-gray-200" />
+        </div>
+        <div className="h-2.5 w-12 rounded-full bg-gray-200" />
+      </div>
+      {Array.from({ length: 15 }).map((_, index) => {
+        return (
+          <div className="flex items-center justify-between pt-4" key={index}>
+            <div>
+              <div className="mb-2.5 h-2.5 w-24 rounded-full bg-gray-200" />
+              <div className="h-2 w-32 rounded-full bg-gray-200" />
+            </div>
+            <div className="h-2.5 w-12 rounded-full bg-gray-200" />
+          </div>
+        );
+      })}
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
 }
