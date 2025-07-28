b8a68885469c9782e2009248dc6071890f9f3f1f
lennardevertz <levertz@idriss.xyz>
2025-07-28 16:06:32 +0200

    add 404 handling

--- apps/main-landing/src/app/creators/obs/[name]/page.tsx
+++ apps/main-landing/src/app/creators/obs/[name]/page.tsx
@@ -1,12 +1,20 @@
+import { notFound } from 'next/navigation';
+
 import Obs from '../page';
+import { getPublicCreatorProfile } from '../../utils';
 
 // ts-unused-exports:disable-next-line
 export default async function CreatorProfile({
   params,
 }: {
-  params: Promise<{ name: string }>;
+  params: { name: string };
 }) {
-  const { name } = await params;
+  const { name } = params;
+  const profile = await getPublicCreatorProfile(name);
+
+  if (!profile) {
+    notFound();
+  }
 
   return <Obs creatorName={name} />;
 }
--- apps/main-landing/src/app/creators/utils/navigation.ts
+++ apps/main-landing/src/app/creators/utils/navigation.ts
@@ -1,3 +1,5 @@
+'use client';
+
 import { useCallback } from 'react';
 import { useRouter } from 'next/navigation';
 import { getAccessToken, usePrivy } from '@privy-io/react-auth';
