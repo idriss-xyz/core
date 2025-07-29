c4cec07a6010bcc78f04c3d037779e4c1db9a024
lennardevertz <levertz@idriss.xyz>
2025-07-29 14:49:26 +0200

    fix type check

--- apps/main-landing/src/app/creators/[name]/page.tsx
+++ apps/main-landing/src/app/creators/[name]/page.tsx
@@ -7,14 +7,14 @@ import { getPublicCreatorProfile } from '../utils';
 import { type CreatorProfile } from '../donate/types';
 
 type Properties = {
-  params: { name: string };
+  params: Promise<{ name: string }>;
 };
 
 // ts-unused-exports:disable-next-line
 export async function generateMetadata({
   params,
 }: Properties): Promise<Metadata> {
-  const { name } = params;
+  const { name } = await params;
   const profile = await getPublicCreatorProfile(name);
   if (!profile) {
     return { robots: { index: false, follow: false } };
@@ -24,7 +24,7 @@ export async function generateMetadata({
 
 // ts-unused-exports:disable-next-line
 export default async function CreatorProfile({ params }: Properties) {
-  const { name } = params;
+  const { name } = await params;
   const rawProfile = await getPublicCreatorProfile(name);
   if (!rawProfile) notFound();
 
