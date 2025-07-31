81a37bebf687ddf641331935ba52eb3e094fda6f
Daniel Arroyo (danielarroyo.eth) <34529009+daniel0ar@users.noreply.github.com>
2025-07-31 08:50:25 -0500

    Revert "fix: Revert QA 100 Skeletons on data load (#633)" (#634)

--- apps/main-landing/src/app/creators/app/ranking/top-creators/page.tsx
+++ apps/main-landing/src/app/creators/app/ranking/top-creators/page.tsx
@@ -6,11 +6,12 @@ import { Hex } from 'viem';
 import { Leaderboard } from '@/app/creators/components/leaderboard';
 
 import { useGetLeaderboard, periodMap } from '../commands/use-get-leaderboard';
+import SkeletonRanking from '../loading';
 
 // ts-unused-exports:disable-next-line
 export default function TopCreators() {
   const [activeFilter, setActiveFilter] = useState('All time');
-  const { user } = usePrivy();
+  const { user, ready, authenticated } = usePrivy();
   const address = user?.wallet?.address as Hex | undefined;
 
   const leaderboardQuery = useGetLeaderboard({
@@ -31,6 +32,10 @@ export default function TopCreators() {
     },
     [leaderboardQuery.data],
   );
+
+  if (leaderboardQuery.isLoading || !ready || !authenticated) {
+    return <SkeletonRanking />;
+  }
 
   return (
     <div>
--- apps/main-landing/src/app/creators/app/ranking/top-donors/page.tsx
+++ apps/main-landing/src/app/creators/app/ranking/top-donors/page.tsx
@@ -6,17 +6,22 @@ import { Hex } from 'viem';
 import { Leaderboard } from '@/app/creators/components/leaderboard';
 
 import { useGetLeaderboard, periodMap } from '../commands/use-get-leaderboard';
+import SkeletonRanking from '../loading';
 
 // ts-unused-exports:disable-next-line
 export default function TopDonors() {
   const [activeFilter, setActiveFilter] = useState('All time');
-  const { user } = usePrivy();
+  const { user, ready, authenticated } = usePrivy();
   const address = user?.wallet?.address as Hex | undefined;
 
   const leaderboardQuery = useGetLeaderboard({
     type: 'donor',
     period: periodMap[activeFilter]!,
   });
+
+  if (leaderboardQuery.isLoading || !ready || !authenticated) {
+    return <SkeletonRanking />;
+  }
 
   return (
     <div>
