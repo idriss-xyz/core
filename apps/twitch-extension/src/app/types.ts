14a202ec60657401349c2e2fece466ea838b61e8
lennardevertz <levertz@idriss.xyz>
2025-08-08 19:15:40 +0200

    update twitch extension

--- apps/twitch-extension/src/app/types.ts
+++ apps/twitch-extension/src/app/types.ts
@@ -1,15 +1,5 @@
-export type FormValues = {
-  address: string;
-};
-
-export type ConfigValues = {
-  address: string;
-};
-
 export type WidgetVariants =
   | 'panel'
   | 'videoOverlay'
   | 'videoComponent'
   | 'creatorsDashboard';
-
-export type ConfigSubmitStatus = 'error' | 'success';
