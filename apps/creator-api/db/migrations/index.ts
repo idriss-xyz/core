9c3583305293d5a1a0649ed6358ae80de5faabc5
Levertz <95511599+lennardevertz@users.noreply.github.com>
2025-08-11 14:55:26 +0200

    fill default tokens and networks for early users (#645)

--- apps/creator-api/db/migrations/index.ts
+++ apps/creator-api/db/migrations/index.ts
@@ -19,3 +19,4 @@ export { AddDoneSetupCreatorField1753315853694 } from './1753315853694-AddDoneSe
 export { AddCreatorAddresses1753656919000 } from './1753656919000-AddCreatorAddresses';
 export { AddForceDonationOverlayRefresh1753315853694 } from './1753315853694-AddForceDonationOverlayRefresh';
 export { AddReceiveEmailsCreatorField1753726864037 } from './1753726864037-AddReceiveEmailsCreatorField';
+export { PopulateCreatorTokenAndNetworkDefaults1754915299000 } from './1754915299000-PopulateCreatorTokenAndNetworkDefaults';
