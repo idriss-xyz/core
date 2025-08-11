e4a79ff6115df5dc0b57105a0a410715449cd767
Daniel <daniel0ar@hotmail.com>
2025-08-11 12:28:31 -0500

    fix: Add ToastProvider

--- apps/main-landing/src/app/creators/providers.tsx
+++ apps/main-landing/src/app/creators/providers.tsx
@@ -4,6 +4,7 @@ import { ReactNode } from 'react';
 import { TooltipProvider } from '@idriss-xyz/ui/tooltip';
 
 import { QueryProvider } from '@/providers';
+import { ToastProvider } from './context/toast-context';
 
 type Properties = {
   children: ReactNode;
@@ -14,7 +15,11 @@ export const Providers = ({ children }: Properties) => {
     <QueryProvider>
       <WithPortal>
         <NiceModal.Provider>
-          <TooltipProvider delayDuration={400}>{children}</TooltipProvider>
+          <TooltipProvider delayDuration={400}>
+            <ToastProvider>
+              {children}
+            </ToastProvider>
+          </TooltipProvider>
         </NiceModal.Provider>
       </WithPortal>
     </QueryProvider>
