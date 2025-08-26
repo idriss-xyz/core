ade9a030dd367185cba4b44cd7aa69540efee05f
Daniel <daniel0ar@hotmail.com>
2025-08-26 18:47:12 -0500

    refactor: Extract available rewards card to separate component

--- apps/main-landing/src/app/creators/app/rewards/invites/page.tsx
+++ apps/main-landing/src/app/creators/app/rewards/invites/page.tsx
@@ -1,47 +1,12 @@
-import { TOKEN } from '@idriss-xyz/constants';
-import { Button } from '@idriss-xyz/ui/button';
-import { Card, CardBody, CardHeader } from '@idriss-xyz/ui/card';
-import { formatFiatValue } from '@idriss-xyz/utils';
-import Image from 'next/image';
+import AvailableRewardsCard from './available-rewards';
 
 // ts-unused-exports:disable-next-line
 export default function InvitesPage() {
-  const { totalAmount } = { totalAmount: 32 }; // TODO: Hook to data (useRewards)
+  const { availableRewards } = { availableRewards: 32 }; // TODO: Hook to data (useRewards)
   return (
     <div>
       <div className="grid grid-cols-3 gap-4">
-        <Card className="col-span-1">
-          <div className="flex flex-col gap-6">
-            <CardHeader className="text-label3 text-neutral-900">
-              Available Rewards
-            </CardHeader>
-            <CardBody className="flex grow items-center">
-              <div className="flex items-center gap-6">
-                <div className="relative size-[70px] rounded-full">
-                  <Image
-                    src={TOKEN.IDRISS.logo}
-                    alt={TOKEN.IDRISS.symbol}
-                    fill
-                    className="rounded-full"
-                  />
-                </div>
-                <div className="flex gap-2">
-                  <div className="flex gap-2">
-                    <span className="flex items-center text-label1 text-black">
-                      {formatFiatValue(totalAmount)}{' '}
-                      <span className="ml-2 text-body4 text-gray-300">
-                        {TOKEN.IDRISS.symbol}
-                      </span>
-                    </span>
-                  </div>
-                  <Button intent="primary" size="medium" suffixIconName="Coins">
-                    Claim
-                  </Button>
-                </div>
-              </div>
-            </CardBody>
-          </div>
-        </Card>
+        <AvailableRewardsCard availableRewards={availableRewards} />
       </div>
     </div>
   );
