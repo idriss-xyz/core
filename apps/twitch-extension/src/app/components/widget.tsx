c2ebe0a80d3aaba79ff68756bb6e352ce13a648d
lennardevertz <levertz@idriss.xyz>
2025-09-01 14:38:51 +0200

    lint

--- apps/twitch-extension/src/app/components/widget.tsx
+++ apps/twitch-extension/src/app/components/widget.tsx
@@ -102,7 +102,6 @@ function WidgetContent({ variant }: ContentProperties) {
 
   useEffect(() => {
     if (donationsHistory.data) {
-
       const allTimeLeaderboard = calculateDonationLeaderboard(
         donationsHistory.data.donations,
       );
