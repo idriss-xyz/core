0da7ffd6512a2d0c6f1999922e1ea73b913c9f15
Daniel <daniel0ar@hotmail.com>
2025-09-05 00:14:24 -0500

    style: Remove console log on scroll

--- apps/main-landing/src/app/creators/components/top-bar.tsx
+++ apps/main-landing/src/app/creators/components/top-bar.tsx
@@ -26,7 +26,6 @@ export const TopBar = ({
 
   useEffect(() => {
     const handleScroll = (event: Event) => {
-      console.log('scrolling');
       const customEvent = event as CustomEvent<{ scrollTop: number }>;
 
       const scrollTop = customEvent.detail.scrollTop;
