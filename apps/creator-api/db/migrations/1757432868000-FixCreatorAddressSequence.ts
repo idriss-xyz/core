b2d777472321eda436777c8bcd1e3c8ea7806fa7
Daniel <daniel0ar@hotmail.com>
2025-09-09 11:40:46 -0500

    docs:  Add comments to index migration. Fix start index.

--- apps/creator-api/db/migrations/1757432868000-FixCreatorAddressSequence.ts
+++ apps/creator-api/db/migrations/1757432868000-FixCreatorAddressSequence.ts
@@ -1,5 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
+// Needed to fix a manual edit on production database
 export class FixCreatorAddressSequence1757432868000
   implements MigrationInterface
 {
@@ -7,12 +8,12 @@ export class FixCreatorAddressSequence1757432868000
     await queryRunner.query(`
       SELECT setval(
         pg_get_serial_sequence('"creator_address"', 'id'),
-        COALESCE((SELECT MAX(id) FROM "creator_address"), 0)
+        COALESCE((SELECT MAX(id) FROM "creator_address"), 1)
       );
     `);
   }
 
   public async down(_queryRunner: QueryRunner): Promise<void> {
-    // no-op
+    // no-op (this is a non reversible change)
   }
 }
