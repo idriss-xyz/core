126f7171814460f383350b8b5005c789898bd532
Daniel <daniel0ar@hotmail.com>
2025-09-09 07:38:26 -0500

    refactor: Change pengy token icon size

--- packages/ui/src/components/icon/custom/token/pengu-token.tsx
+++ packages/ui/src/components/icon/custom/token/pengu-token.tsx
@@ -4,9 +4,7 @@ export const PenguToken = (properties: SVGProps<SVGSVGElement>) => {
   return (
     <svg
       {...properties}
-      width="60"
-      height="60"
-      viewBox="0 0 60 60"
+      viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
