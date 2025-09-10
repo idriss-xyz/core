32d44cc410a1045bdad846de4a54fd6141b6df95
lennardevertz <levertz@idriss.xyz>
2025-09-10 12:10:01 +0200

    fix padding of selector arrows

--- apps/main-landing/src/app/creators/app/setup/select/select.component.tsx
+++ apps/main-landing/src/app/creators/app/setup/select/select.component.tsx
@@ -79,7 +79,7 @@ export const Select = <T,>({
               </div>
             )}
 
-            <div className="flex-1 overflow-hidden px-3">
+            <div className="flex-1 overflow-hidden pl-3">
               <SelectInput
                 placeholder={placeholder}
                 value={pickedOption.label}
