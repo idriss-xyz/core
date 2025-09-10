e09e05bbe7334f516cf25ca82e89905c383b5f41
Daniel Arroyo (danielarroyo.eth) <34529009+daniel0ar@users.noreply.github.com>
2025-09-10 15:42:20 -0500

    fix: Redirect if not authenticated on creators app (#690)

--- apps/main-landing/src/app/creators/hooks/use-logout.ts
+++ apps/main-landing/src/app/creators/hooks/use-logout.ts
@@ -1,21 +1,13 @@
 import { usePrivy } from '@privy-io/react-auth';
-import { useRouter } from 'next/navigation';
 
 import { useAuth } from '../context/auth-context';
 
 export const useLogout = () => {
   const { logout } = usePrivy();
-  const router = useRouter();
-  const { setCreator, setDonor, setIsModalOpen, callbackUrl } = useAuth();
+  const { setCreator, setIsModalOpen, setDonor } = useAuth();
 
   return async () => {
     await logout();
-
-    if (!callbackUrl || callbackUrl?.endsWith('/creators')) {
-      router.replace('/creators');
-    } else {
-      router.replace(callbackUrl);
-    }
 
     localStorage.removeItem('twitch_new_user_info');
     localStorage.removeItem('custom-auth-token');
