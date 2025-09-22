c5d685ee6c30191ea7486c26c95b811accd59aa4
Daniel <daniel0ar@hotmail.com>
2025-09-22 13:26:46 -0500

    ci: Format logs

--- apps/main-landing/src/app/creators/components/top-bar/desktop/desktop.tsx
+++ apps/main-landing/src/app/creators/components/top-bar/desktop/desktop.tsx
@@ -51,9 +51,9 @@ export const Desktop = ({
   };
 
   useEffect(() => {
-    console.log("user", user);
+    console.log('user', user);
     if (user && localStorage.getItem('donate-option-choice') === 'account') {
-      console.log("SETTING DONOR")
+      console.log('SETTING DONOR');
       void setCreatorIfSessionPresent(user, setDonor);
     }
   }, [user, setDonor]);
