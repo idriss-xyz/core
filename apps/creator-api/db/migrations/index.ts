cf7a239f6ff0a65c352388fe916797420d1673f9
Daniel Arroyo (danielarroyo.eth) <34529009+daniel0ar@users.noreply.github.com>
2025-09-23 04:50:25 -0500

    Fix: Keep donor session between tabs (QA 149) (#700)

--- apps/creator-api/db/migrations/index.ts
+++ apps/creator-api/db/migrations/index.ts
@@ -26,3 +26,4 @@ export { RenameEtherToEthereum1755609037000 } from './1755609037000-RenameEtherT
 export { AddIsDonorToCreator1757184858317 } from './1757184858317-AddIsDonorToCreator';
 export { FixCreatorAddressSequence1757432868000 } from './1757432868000-FixCreatorAddressSequence';
 export { CleanupUnsupportedTokensNetworks1757496093000 } from './1757496093000-CleanupUnsupportedTokensNetworks';
+export { RemoveWwwFromUrls1758590345135 } from './1758590345135-RemoveWwwFromUrls';
