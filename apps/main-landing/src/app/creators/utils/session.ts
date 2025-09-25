04aa3749415df49aea5b0f2b331a9b4d4ae54c8c
Daniel <daniel0ar@hotmail.com>
2025-09-25 11:16:16 -0500

    fix: Server util functions on separate file

--- apps/main-landing/src/app/creators/utils/session.ts
+++ apps/main-landing/src/app/creators/utils/session.ts
@@ -28,45 +28,6 @@ export const getCreatorProfile = async (
   return data;
 };
 
-export const getPublicCreatorProfile = async (
-  name?: string | null,
-  address?: Hex | null,
-): Promise<CreatorProfileResponse | undefined> => {
-  if (!name && !address) {
-    console.error('No name or address to get creator');
-    return;
-  }
-
-  const lookPath = address ? `address/${address}` : `${name}`;
-
-  const response = await fetch(
-    `${CREATOR_API_URL}/creator-profile/${lookPath}`,
-  );
-  if (!response.ok) {
-    return;
-  }
-  const data = (await response.json()) as CreatorProfileResponse;
-  return data;
-};
-
-export const getPublicCreatorProfileBySlug = async (
-  slug: string | null,
-): Promise<CreatorProfileResponse | undefined> => {
-  if (!slug) {
-    console.error('No slug to get creator');
-    return;
-  }
-
-  const response = await fetch(
-    `${CREATOR_API_URL}/creator-profile/donation-overlay/${slug}`,
-  );
-  if (!response.ok) {
-    return;
-  }
-  const data = (await response.json()) as CreatorProfileResponse;
-  return data;
-};
-
 export const saveCreatorProfile = async (
   address: Hex,
   name?: string | null,
