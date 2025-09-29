7e269d06141ce27c396439c1604759dfa33fa74c
Daniel <daniel0ar@hotmail.com>
2025-09-29 09:45:15 -0500

    fix: "Creator not found" 404 page on mobile (QA 168)

--- apps/main-landing/src/app/creators/not-found.tsx
+++ apps/main-landing/src/app/creators/not-found.tsx
@@ -7,12 +7,12 @@ export default function NotFound() {
   return (
     <>
       <TopBar />
-      <main className="relative flex min-h-screen grow flex-col items-center justify-around gap-4 overflow-hidden bg-[radial-gradient(181.94%_192.93%_at_16.62%_0%,_#E7F5E7_0%,_#76C282_100%)] px-2 pb-1 pt-[56px] lg:flex-row lg:items-start lg:justify-center lg:px-0">
+      <main className="relative flex min-h-screen grow flex-col text-center items-center justify-around gap-4 overflow-hidden bg-[radial-gradient(181.94%_192.93%_at_16.62%_0%,_#E7F5E7_0%,_#76C282_100%)] px-2 pb-1 pt-[56px] lg:flex-row lg:items-start lg:justify-center lg:px-0">
         <div className="flex h-[80vh] flex-col items-center justify-center gap-3">
-          <h1 className="text-display2 uppercase text-neutral-900">
+          <h1 className="text-display4 md:text-display2 uppercase text-neutral-900">
             Creator not found
           </h1>
-          <span className="text-body2">
+          <span className="text-body4 md:text-body2">
             We couldn’t find a creator with that name. Check the spelling and
             try again.
           </span>
