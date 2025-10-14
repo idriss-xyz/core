3a7c437c494ad15d6c0966e5ec8b304f79e4d955
Daniel <daniel0ar@hotmail.com>
2025-10-14 12:45:11 -0500

    refactor: Rename donation overlay page

--- apps/main-landing/src/app/creators/donation-overlay/[slug]/page.tsx
+++ apps/main-landing/src/app/creators/donation-overlay/[slug]/page.tsx
@@ -4,7 +4,7 @@ import DonationOverlay from '../donation-overlay';
 import { getPublicCreatorProfileBySlug } from '../../utils';
 
 // ts-unused-exports:disable-next-line
-export default async function CreatorProfile({
+export default async function DonationOverlayPage({
   params,
 }: {
   params: Promise<{ slug: string }>;
