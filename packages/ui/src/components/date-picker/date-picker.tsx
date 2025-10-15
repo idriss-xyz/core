66b42fce18661d9e76f601cad6f211c20b5dd5cd
Daniel <daniel0ar@hotmail.com>
2025-10-15 12:22:20 -0500

    style: Modify datepicker styles

--- packages/ui/src/components/date-picker/date-picker.tsx
+++ packages/ui/src/components/date-picker/date-picker.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import * as Popover from '@radix-ui/react-popover';
-import { DayPicker } from 'react-day-picker';
+import { DayPicker, getDefaultClassNames } from 'react-day-picker';
 import 'react-day-picker/style.css';
 
 interface DatePickerProperties {
@@ -14,6 +14,8 @@ export const DatePicker = ({
   onSelect,
   children,
 }: DatePickerProperties) => {
+  const defaultClassNames = getDefaultClassNames();
+
   return (
     <Popover.Root>
       <Popover.Trigger asChild>{children}</Popover.Trigger>
@@ -23,7 +25,17 @@ export const DatePicker = ({
           sideOffset={8}
           className="w-auto rounded-md border border-gray-200 bg-white p-2 shadow-md"
         >
-          <DayPicker mode="single" selected={date} onSelect={onSelect} />
+          <DayPicker
+            mode="single"
+            selected={date}
+            onSelect={onSelect}
+            classNames={{
+              today: `$bg-mint-500 rounded-full text-mint-500`,
+              selected: `bg-mint-500 rounded-full text-white`,
+              root: `${defaultClassNames.root} shadow-none`,
+              chevron: `fill-black`,
+            }}
+          />
           <Popover.Arrow className="fill-white" />
         </Popover.Content>
       </Popover.Portal>
