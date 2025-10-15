d767118353b095056d7cd3568bd6f23cea680d22
Daniel <daniel0ar@hotmail.com>
2025-10-14 22:26:51 -0500

    feat: Add date picker component

---
+++ packages/ui/src/components/date-picker/date-picker.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react'
+import { Popover } from "@radix-ui/react-popover"
+
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/style.css";
+
+interface DatePickerProps {
+  date?: Date
+  onSelect?: (date: Date | undefined) => void
+  children: ReactNode
+}
+
+export const DatePicker = ({ date, onSelect, children }: DatePickerProps) => {
+  return (
+    <Popover.Root>
+      <Popover.Trigger asChild>
+        {children}
+      </Popover.Trigger>
+
+      <Popover.Portal>
+        <Popover.Content
+          sideOffset={8}
+          className="w-auto rounded-md border border-gray-200 bg-white p-2 shadow-md"
+        >
+          <DayPicker mode="single" selected={date} onSelect={onSelect} />
+          <Popover.Arrow className="fill-white" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
+  )
+}
