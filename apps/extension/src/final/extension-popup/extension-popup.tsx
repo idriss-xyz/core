44e435fb95d90b3db6d57150aebb25786c1b32f7
lennardevertz <levertz@idriss.xyz>
2025-10-22 11:14:16 +0200

    change popup

--- apps/extension/src/final/extension-popup/extension-popup.tsx
+++ apps/extension/src/final/extension-popup/extension-popup.tsx
@@ -2,21 +2,15 @@ import { Route, Routes } from 'react-router';
 
 import { Closable } from 'shared/ui';
 import { POPUP_ROUTE, useExtensionPopup } from 'shared/extension';
-import { SubscriptionsManagement } from 'application/trading-copilot';
-import { LINES, LINES2 } from 'assets/images';
+import { LINES } from 'assets/images';
 
 import {
-  GovernanceSettings,
-  MainSettings,
-  OtherSettings,
   Products,
 } from './views';
 import {
   TopBar,
   Footer,
   PopupContentLayout,
-  SettingsLayout,
-  TradingCopilotLayout,
 } from './components';
 
 export const ExtensionPopup = () => {
@@ -38,26 +32,6 @@ export const ExtensionPopup = () => {
       <Routes>
         <Route element={<PopupContentLayout backgroundImage={LINES} />}>
           <Route path={POPUP_ROUTE.PRODUCTS} element={<Products />} />
-        </Route>
-        <Route element={<PopupContentLayout backgroundImage={LINES2} />}>
-          <Route element={<SettingsLayout />}>
-            <Route path={POPUP_ROUTE.SETTINGS} element={<MainSettings />} />
-            <Route
-              path={POPUP_ROUTE.GOVERNANCE_SETTINGS}
-              element={<GovernanceSettings />}
-            />
-            <Route
-              path={POPUP_ROUTE.OTHER_SETTINGS}
-              element={<OtherSettings />}
-            />
-          </Route>
-
-          <Route element={<TradingCopilotLayout />}>
-            <Route
-              path={POPUP_ROUTE.TRADING_COPILOT}
-              element={<SubscriptionsManagement />}
-            />
-          </Route>
         </Route>
       </Routes>
 
--- apps/extension/src/final/extension-popup/views/products/products.tsx
+++ apps/extension/src/final/extension-popup/views/products/products.tsx
@@ -1,118 +1,26 @@
-import { Icon } from '@idriss-xyz/ui/icon';
-import { ExternalLink } from '@idriss-xyz/ui/external-link';
 import { Button } from '@idriss-xyz/ui/button';
-import { useCallback } from 'react';
-import { ANNOUNCEMENT_LINK } from '@idriss-xyz/constants';
-
-import {
-  AGORA_LOGO,
-  IDRISS_COIN,
-  POLYMARKET_LOGO,
-  SNAPSHOT_LOGO,
-  TALLY_LOGO,
-} from 'assets/images';
-import { POPUP_ROUTE, useExtensionPopup } from 'shared/extension';
-
-import { ProductCard } from './product-card';
 
 export const Products = () => {
-  const popup = useExtensionPopup();
-
-  const goToSettings = useCallback(() => {
-    popup.navigate(POPUP_ROUTE.SETTINGS);
-  }, [popup]);
-
-  const goToTradingCopilot = useCallback(() => {
-    popup.navigate(POPUP_ROUTE.TRADING_COPILOT);
-  }, [popup]);
 
   return (
     <div className="bg-mint-100 px-4 py-6">
       <h1 className="text-center text-heading5 text-neutralGreen-900">
-        {`GM, it's your web3 copilot speaking`}
+        {`We're moving on to bigger things`}
       </h1>
       <p className="mt-2 text-center text-label4 text-neutralGreen-700">
-        Explore your favorite features below
+        Now building the best onchain<br />
+        monetization app for streamers
       </p>
-      <div className="mt-10 grid grid-cols-2 gap-3">
-        <ExternalLink href={ANNOUNCEMENT_LINK.TIPPING}>
-          <ProductCard
-            heading={<img src={IDRISS_COIN} alt="" className="size-10" />}
-            title={
-              <ProductCard.Title className="mt-4">
-                Tipping Badges
-              </ProductCard.Title>
-            }
-            description={
-              <ProductCard.Description className="mt-1">
-                Send crypto on X and Farcaster
-              </ProductCard.Description>
-            }
-          />
-        </ExternalLink>
-
-        <ExternalLink href={ANNOUNCEMENT_LINK.SNAPSHOT}>
-          <ProductCard
-            heading={
-              <div className="flex space-x-2">
-                <img src={AGORA_LOGO} alt="" className="size-10" />
-                <img src={TALLY_LOGO} alt="" className="size-10" />
-                <img src={SNAPSHOT_LOGO} alt="" className="size-10" />
-              </div>
-            }
-            title={
-              <ProductCard.Title className="mt-4">
-                Governance Widget
-              </ProductCard.Title>
-            }
-            description={
-              <ProductCard.Description className="mt-1">
-                Get alerts for active DAO votes
-              </ProductCard.Description>
-            }
-          />
-        </ExternalLink>
-
-        <ExternalLink href={ANNOUNCEMENT_LINK.POLYMARKET}>
-          <ProductCard
-            heading={<img src={POLYMARKET_LOGO} alt="" className="size-10" />}
-            title={
-              <ProductCard.Title className="mt-4">
-                Polymarket Widget
-              </ProductCard.Title>
-            }
-            description={
-              <ProductCard.Description className="mt-1">
-                Trade directly from your X feed
-              </ProductCard.Description>
-            }
-          />
-        </ExternalLink>
-
-        <div className="hover:cursor-pointer" onClick={goToTradingCopilot}>
-          <ProductCard
-            heading={<Icon name="Rocket" size={40} className="text-black" />}
-            title={
-              <ProductCard.Title className="mt-4">
-                Trading Copilot
-              </ProductCard.Title>
-            }
-            description={
-              <ProductCard.Description className="mt-1">
-                Copy onchain moves
-              </ProductCard.Description>
-            }
-          />
-        </div>
-      </div>
-
       <Button
         intent="primary"
         size="medium"
+        isExternal
+        asLink
         className="mx-auto mt-10"
-        onClick={goToSettings}
+        href='https://idriss.xyz'
+        suffixIconName='IdrissArrowRight'
       >
-        CUSTOMIZE YOUR EXPERIENCE
+        LEARN MORE
       </Button>
     </div>
   );
