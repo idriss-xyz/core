b7ecc4d06e5394a75182eb5a10939df9bf565398
lennardevertz <levertz@idriss.xyz>
2025-11-12 12:29:42 +0100

    add fallback suggested referrals

--- packages/utils/src/server/twitch-api.ts
+++ packages/utils/src/server/twitch-api.ts
@@ -30,11 +30,138 @@ export interface FollowedChannel {
 // ts-unused-exports:disable-next-line
 export const DEFAULT_FOLLOWED_CHANNELS: FollowedChannel[] = [
   {
-    broadcasterId: '000000',
-    login: 'example_streamer',
-    name: 'Example Streamer',
-    profileImage: '',
-    followers: 0,
+    broadcasterId: '96694318',
+    login: 'itokatv',
+    name: 'itokatv',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/61267e76-da41-406f-bb5d-114c759661ff-profile_image-70x70.jpg',
+    followers: 75_710,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '844609866',
+    login: 'threadguy',
+    name: 'ThreadGuy',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/36375bf2-fec0-4fb3-a5f2-02575bb63325-profile_image-70x70.png',
+    followers: 30_692,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '416012132',
+    login: 'andyslaps',
+    name: 'AndySlaps',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/69c5e024-9287-46fa-b0c6-3dbc25ed422c-profile_image-70x70.png',
+    followers: 22_836,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '46168728',
+    login: 'rojankhzxr',
+    name: 'Rojankhzxr',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/7787858f-9b07-4857-9cec-d64c4f341658-profile_image-70x70.png',
+    followers: 199_137,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '1142457292',
+    login: 'gunnygopher',
+    name: 'GunnyGopher',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/955019e2-3389-4ebf-9c27-e92a1d9cbb80-profile_image-70x70.png',
+    followers: 1194,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '169265076',
+    login: 'stormingh',
+    name: 'Stormingh',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/422b2299-3e2a-41a5-b33a-e2a8e31c7898-profile_image-70x70.png',
+    followers: 30_171,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '146922206',
+    login: 'imperialhal__',
+    name: 'ImperialHal__',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/6d306649-dd15-4db8-82d8-655694ba692f-profile_image-70x70.png',
+    followers: 2_068_159,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '233300375',
+    login: 'clix',
+    name: 'Clix',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/f700cd74-e74c-42a4-ba5b-18efb486eb92-profile_image-70x70.png',
+    followers: 8_414_937,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '88750392',
+    login: 'yagodnft',
+    name: 'yagodnft',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/2135bd36-be4e-41bd-a91e-e7f2065b2f89-profile_image-70x70.png',
+    followers: 1996,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '36769016',
+    login: 'timthetatman',
+    name: 'TimTheTatman',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/7dcd89d5-6700-4b05-b9c9-c7dac61c32db-profile_image-70x70.png',
+    followers: 7_593_005,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '19571641',
+    login: 'ninja',
+    name: 'Ninja',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/90d40495-f467-4911-9035-72d8d10a49c5-profile_image-70x70.png',
+    followers: 19_235_201,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '97599849',
+    login: 'theohs_',
+    name: 'TheoHS_',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/29720f1f-dc30-4bd4-96ff-864be515eeea-profile_image-70x70.jpeg',
+    followers: 61_653,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '159176262',
+    login: 'menyeb',
+    name: 'menyeB',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/e200012e-9cb8-4eac-8fb7-3e0b736f8748-profile_image-70x70.png',
+    followers: 31_527,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '555802124',
+    login: 'melabeeofficial',
+    name: 'MelaBeeOfficial',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/8cca933b-f92d-4673-8dc9-c77535b0c0b9-profile_image-70x70.png',
+    followers: 4255,
+    followedAt: new Date(0).toISOString(),
+  },
+  {
+    broadcasterId: '10815652',
+    login: 'fwiz',
+    name: 'Fwiz',
+    profileImage:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/fwiz-profile_image-ef56f4c10db9e9eb-70x70.jpeg',
+    followers: 80_402,
     followedAt: new Date(0).toISOString(),
   },
 ];
