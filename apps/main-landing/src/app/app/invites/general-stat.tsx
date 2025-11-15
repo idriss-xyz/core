d9c0394c36f91ef092f03eebad056e4dba2b6160
lennardevertz <levertz@idriss.xyz>
2025-11-15 18:11:20 -0300

    add gradient circle to general stat card

--- apps/main-landing/src/app/app/invites/general-stat.tsx
+++ apps/main-landing/src/app/app/invites/general-stat.tsx
@@ -1,5 +1,7 @@
 import { Card, CardBody, CardHeader } from '@idriss-xyz/ui/card';
 
+import { GradientCircle } from '@/app/components/invite-section';
+
 type Properties = {
   header: string;
   stat: string;
@@ -8,20 +10,12 @@ type Properties = {
 // ts-unused-exports:disable-next-line
 export default function GeneralStatCard({ header, stat }: Properties) {
   return (
-    <Card className="col-span-1">
+    <Card className="col-span-1 space-y-3">
       <CardHeader className="text-label3 text-neutral-900">{header}</CardHeader>
-      {/* centre content and wrap it with the “glow” circle */}
-      <CardBody className="flex size-full items-center justify-center">
-        <div
-          className="flex h-[96px] w-[94px] items-center justify-center rounded-[1200px] bg-[linear-gradient(0deg,rgba(255,255,255,0.25)_0%,rgba(255,255,255,0.05)_100%)] opacity-100"
-          style={{
-            borderTopLeftRadius: 1000,
-            borderTopRightRadius: 1000,
-            borderBottomRightRadius: 1200,
-            borderBottomLeftRadius: 1200,
-          }}
-        >
-          <h3 className="text-heading4 text-neutral-900">{stat}</h3>
+      <CardBody className="flex items-center justify-center">
+        <div className="relative flex size-24 items-center justify-center">
+          <GradientCircle className="size-full" />
+          <h3 className="absolute text-heading4 text-neutral-900">{stat}</h3>
         </div>
       </CardBody>
     </Card>
