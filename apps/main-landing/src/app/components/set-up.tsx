4ef050fed468b546e2373e90702a834ef82a205f
lennardevertz <levertz@idriss.xyz>
2026-02-06 16:44:46 +0100

    move lines

--- apps/main-landing/src/app/components/set-up.tsx
+++ apps/main-landing/src/app/components/set-up.tsx
@@ -153,7 +153,7 @@ export const SetUp = () => {
               <img
                 alt=""
                 src={CREATORS_LINES.src}
-                className="absolute right-12 top-0 hidden h-full object-contain lg:block xl:right-16"
+                className="absolute right-32 top-0 hidden h-full object-contain lg:block xl:right-40"
               />
             </div>
           </div>
